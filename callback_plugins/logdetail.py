d6d35dea64ab15a254e009ec9248bb53e1c16c3d
Seth Vidal <skvidal@fedoraproject.org>
2013-07-03 17:05:07 +0000

    add info on who wrote this

--- callback_plugins/logdetail.py
+++ callback_plugins/logdetail.py
@@ -1,7 +1,7 @@
 # (C) 2012, Michael DeHaan, <michael.dehaan@gmail.com>
+# based on the log_plays example
+# skvidal@fedoraproject.org
 
-# This file is part of Ansible
-#
 # Ansible is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
