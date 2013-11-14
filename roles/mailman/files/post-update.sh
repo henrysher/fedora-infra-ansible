7bae9777c4c3912adf6f977a7b842e1154a5adf3
Aurélien Bompard <aurelien@bompard.org>
2013-11-14 00:33:09 +0000

    mailman: fix SELinux context issues
    And run the post-update script in a yum-post-transaction action.

--- roles/mailman/files/post-update.sh
+++ roles/mailman/files/post-update.sh
@@ -8,6 +8,7 @@ export PATH=$PATH:$(dirname $(realpath $0)) # make yamlget available
 
 BASEDIR=`yamlget basedir $CONFFILE`
 CONFDIR=`yamlget confdir $CONFFILE`
+INDEXDIR=$BASEDIR/kittystore_search_index
 
 django-admin collectstatic --clear --noinput --pythonpath $CONFDIR --settings settings
 django-admin assets build --parse-templates --pythonpath $CONFDIR --settings settings
@@ -15,6 +16,8 @@ django-admin syncdb --pythonpath $CONFDIR --settings settings_admin
 django-admin migrate hyperkitty --pythonpath $CONFDIR --settings settings_admin
 django-admin loaddata /etc/postorius/sites/default/initial-user.json --pythonpath $CONFDIR --settings settings_admin
 kittystore-updatedb --pythonpath $CONFDIR --settings settings_admin
+chown mailman:mailman -R $INDEXDIR
+chmod g+w -R $INDEXDIR
 
 # Give database rights to the non-admin user
 $BASEDIR/bin/pg-give-rights.py
