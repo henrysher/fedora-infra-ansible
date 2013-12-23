002135eb990b71bad9f3243c259f8ea735e3a0e8
Andrea Veri <averi@fedoraproject.org>
2013-12-23 16:08:29 +0000

    Move some of the configurations bits on the SSH config file and drop them from the backup script

--- files/gnome/backup.sh
+++ files/gnome/backup.sh
@@ -32,12 +32,11 @@ MACHINES='signal.gnome.org
 
 BACKUP_DIR='/fedora_backups/gnome/'
 LOGS_DIR='/fedora_backups/gnome/logs'
-BACKUP_USER='root'
 
 for MACHINE in $MACHINES; do
-      rsync -avz -e "ssh -F /usr/local/etc/gnome_ssh_config -i /usr/local/etc/gnome_backup_id.rsa" --bwlimit=2000 $BACKUP_USER@$MACHINE:/etc/rsyncd/backup.exclude $BACKUP_DIR/excludes/$MACHINE.exclude
+      rsync -avz -e "ssh -F /usr/local/etc/gnome_ssh_config" --bwlimit=2000 $MACHINE:/etc/rsyncd/backup.exclude $BACKUP_DIR/excludes/$MACHINE.exclude
       cd $BACKUP_DIR/$MACHINE
-      rsync -avz -e "ssh -F /usr/local/etc/gnome_ssh_config -i /usr/local/etc/gnome_backup_id.rsa" --bwlimit=2000 --exclude-from=$BACKUP_DIR/excludes/$MACHINE.exclude --log-file=$LOGS_DIR/$MACHINE.log $BACKUP_USER@$MACHINE:/ .
+      rsync -avz -e "ssh -F /usr/local/etc/gnome_ssh_config" --bwlimit=2000 --exclude-from=$BACKUP_DIR/excludes/$MACHINE.exclude --log-file=$LOGS_DIR/$MACHINE.log $MACHINE:/ .
 done
 
 
