7258e676a6f14f95288cf780547fbc343922738e
Andrea Veri <averi@fedoraproject.org>
2013-12-28 17:58:47 +0000

    GNOME Backups: don't assign the rdiff-backup's schemas to a variable

--- files/gnome/backup.sh
+++ files/gnome/backup.sh
@@ -32,10 +32,9 @@ MACHINES='signal.gnome.org
 
 BACKUP_DIR='/fedora_backups/gnome/'
 LOGS_DIR='/fedora_backups/gnome/logs'
-SCHEMA='ssh -F /usr/local/etc/gnome_ssh_config %s rdiff-backup --server'
 
 for MACHINE in $MACHINES; do
       rsync -avz -e 'ssh -F /usr/local/etc/gnome_ssh_config' --bwlimit=2000 $MACHINE:/etc/rsyncd/backup.exclude $BACKUP_DIR/excludes/$MACHINE.exclude
       cd $BACKUP_DIR/$MACHINE
-      rdiff-backup --force --remote-schema '$SCHEMA' --print-statistics --exclude-device-files --exclude-filelist $BACKUP_DIR/excludes/$MACHINE.exclude $MACHINE:/ . | mail backups@gnome.org
+      rdiff-backup --force --remote-schema 'ssh -F /usr/local/etc/gnome_ssh_config %s rdiff-backup --server' --print-statistics --exclude-device-files --exclude-filelist $BACKUP_DIR/excludes/$MACHINE.exclude $MACHINE:/ . | mail backups@gnome.org
 done
