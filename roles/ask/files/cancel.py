da98f8f0f383346937937f692eb5888c7ba9e553
Kevin Fenzi <kevin@scrye.com>
2014-01-21 18:00:36 +0000

    Try a hotfix for the broken feedback issue.

--- roles/ask/files/cancel.py
+++ roles/ask/files/cancel.py
@@ -8,7 +8,7 @@ class CancelActionMiddleware(object):
                 msg = getattr(view_func,'CANCEL_MESSAGE')
             except AttributeError:
                 msg = 'action canceled'
-            request.user.message_set.create(message=msg)
+            request.user.message_set.create(message=unicode(msg))
             return HttpResponseRedirect(get_next_url(request))
         else:
             return None
