1a791a6e0ea96b075a2d4d59a65a6b1f48a80cd5
Ralph Bean <rbean@redhat.com>
2014-01-23 21:06:02 +0000

    Point the fmn frontend at datanommer.

--- roles/notifs-frontend/templates/fmn.web.py
+++ roles/notifs-frontend/templates/fmn.web.py
@@ -1,3 +1,6 @@
 config = {
+    # This is for *our* database
     "fmn.sqlalchemy.uri": "postgresql://{{notifs_db_user}}:{{notifs_db_password}}@db-notifs/notifications",
+    # And this is for the datanommer database
+    "datanommer.sqlalchemy.url": "postgresql://{{datanommerDBUser}}:{{datanommerDBPassword}}@db-datanommer/datanommer",
 }
