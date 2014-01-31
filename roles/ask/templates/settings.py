5ee49f57fb8b66496b5a2a92f833b59a3953021e
Kevin Fenzi <kevin@scrye.com>
2014-01-31 22:31:33 +0000

    This should just be zh I think.

--- roles/ask/templates/settings.py
+++ roles/ask/templates/settings.py
@@ -255,7 +255,7 @@ LANGUAGES = (
     ('es', gettext('Spanish')),
     ('en', gettext('English')),
     ('ru', gettext('Russian')),
-    ('zh-tw', gettext('Chinese')),
+    ('zh', gettext('Chinese')),
     ('fr', gettext('French')),
     ('el', gettext('Greek')),
     ('id', gettext('Indonesian')),
