e258bae72737bbfc01956de1b890849e12ca1835
Ralph Bean <rbean@redhat.com>
2014-05-29 16:36:43 +0000

    Collectd+postgres for db-datanommer01

---
+++ roles/collectd/postgres/templates/collectd-postgres.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+pause=10
+host=$( hostname -s )
+
+while getopts "h:p:s:" c; do
+       case $c in
+               h)      host=$OPTARG;;
+               p)      pause=$OPTARG;;
+               *)      echo "Usage: /bin/bash [-h <hostname>] [-p <seconds>]";;
+       esac
+done
+
+
+ while [ $? -eq 0 ] ; do
+   time="$(date +%s)"
+   for db in {{ databases.join(' ') }} 
+   do
+      echo "PUTVAL $host/pg_conns/pg_conns-$db $time:$(ps auxwww | grep -c $db)"
+   done
+   sleep $pause
+ done
