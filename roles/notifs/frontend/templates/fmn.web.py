3bd8b1ffa34e27c19565e7866bdc9fe95d25641a
Ralph Bean <rbean@redhat.com>
2014-06-11 13:35:03 +0000

    fmn.web needs to know about this too.

--- roles/notifs/frontend/templates/fmn.web.py
+++ roles/notifs/frontend/templates/fmn.web.py
@@ -3,4 +3,6 @@ config = {
     "fmn.sqlalchemy.uri": "postgresql://{{notifs_db_user}}:{{notifs_db_password}}@db-notifs/notifications",
     # And this is for the datanommer database
     "datanommer.sqlalchemy.url": "postgresql://{{datanommerDBUser}}:{{datanommerDBPassword}}@db-datanommer/datanommer",
+
+    "fmn.backends": ["irc", "email"],  # But "android" is disabled.
 }
