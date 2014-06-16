0458fc105b587331af7b9288f11c9562b3bb2852
Ralph Bean <rbean@redhat.com>
2014-06-16 20:20:21 +0000

    Start of a github2fedmsg role.

---
+++ roles/github2fedmsg/templates/githubsecrets.py
@@ -0,0 +1,23 @@
+# This file is a part of github2fedmsg, a pubsubhubbub to zeromq bridge.
+# Copyright (C) 2014, Red Hat, Inc.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+# github2fedmsg's secret key to the kingdom
+{% if env != 'staging' %}
+secret_oauth_token = "{{github2fedmsgGHsecret_oauth_token}}"
+{% else %}
+secret_oauth_token = "{{github2fedmsgGHsecret_oauth_token_staging}}"
+{% endif %}
+
