abedfb7cc8901cbd217a18dba177ac58cd34105e
Kevin Fenzi <kevin@scrye.com>
2014-07-14 22:16:47 +0000

    Switch stuff over to new memcached

--- roles/ask/templates/settings.py
+++ roles/ask/templates/settings.py
@@ -268,7 +268,7 @@ CACHES = {
         'BACKEND': 'django.core.cache.backends.memcached.MemcachedCache',
         'KEY_PREFIX': 'askfedora',
         'LOCATION': [
-            'memcached04:11211',
+            'memcached02:11211',
         ]
     }
 }
