45e5b2536f4d11530b0bead9bfc65138037faa24
Ralph Bean <rbean@redhat.com>
2014-07-17 01:52:44 +0000

    Yes.  PY2 is the same as 'not PY3'.

--- roles/fedmsg/hub/files/websockets.py
+++ roles/fedmsg/hub/files/websockets.py
@@ -1,6 +1,9 @@
-
 config = {
     # The presence of this will cause fedmsg-hub to start its own websocket
     # server along with it.
     'moksha.livesocket.websocket.port': 9919,
 }
+
+# And... this is a hack to get python-txws to work with python-six on epel7
+import six
+six.PY2 = not six.PY3
