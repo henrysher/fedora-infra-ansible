fc58f89ab9d68f42d11c319392330c4d93eb2ca5
Ralph Bean <rbean@redhat.com>
2014-07-19 13:29:35 +0000

    Typo.

--- roles/notifs/backend/templates/fmn.consumer.py
+++ roles/notifs/backend/templates/fmn.consumer.py
@@ -25,7 +25,7 @@ config = {
     # This sets up four threads to handle incoming messages.  At the time of
     # this commit, all of our fedmsg daemons are running in single-threaded
     # mode.  If we turn it on globally, we should remove this setting.
-    "moksha.threads_per_consumer": 4,
+    "moksha.workers_per_consumer": 4,
 
     # Some configuration for the rule processors
     "fmn.rules.utils.use_pkgdb2": True,
