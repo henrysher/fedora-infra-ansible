fcb92e3dbfe439795bee725f0b1112e8f2fc9969
Luke Macken <lmacken@redhat.com>
2014-10-30 19:27:20 +0000

    atomic-composer: Add a custom config

---
+++ roles/atomic-composer/files/config.py
@@ -0,0 +1,15 @@
+config = dict(
+    trees=['rawhide', 'f21'],
+    fedmsg_atomic_composer=True,
+    config_key='fedmsg_atomic_composer',
+    topic = ['org.fedoraproject.prod.bodhi.updates.fedora.sync',
+             'org.fedoraproject.prod.compose.branched.rsync.complete',
+             'org.fedoraproject.prod.compose.rawhide.rsync.complete',
+             'org.fedoraproject.stg.bodhi.updates.fedora.sync',
+             'org.fedoraproject.stg.compose.branched.rsync.complete',
+             'org.fedoraproject.stg.compose.rawhide.rsync.complete'],
+    touch_dir='/srv/inbox/',
+    output_dir='/srv/fedora-atomic/output/',
+    production_repos='localhost:/srv/production/',
+    local_repos='/srv/fedora-atomic/',
+)
