e663b4140c8b15c1c3327e6bd5bffb368fcea4e9
Kevin Fenzi <kevin@scrye.com>
2014-11-16 02:24:28 +0000

    Take out combobox so we can check/fix/prune it.

--- files/gnome/backup.sh
+++ files/gnome/backup.sh
@@ -20,7 +20,6 @@ MACHINES='signal.gnome.org
           bastion.gnome.org
           spinner.gnome.org
           master.gnome.org
-          combobox.gnome.org
           restaurant.gnome.org
           expander.gnome.org
           live.gnome.org
