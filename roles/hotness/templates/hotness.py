31e9f72fddf92fd36e2421c87ab2f07f65f212d5
Ralph Bean <rbean@redhat.com>
2014-11-17 19:30:42 +0000

    Initial stubbing for the new hotness node(s).

---
+++ roles/hotness/templates/hotness.py
@@ -0,0 +1,59 @@
+import os
+import socket
+hostname = socket.gethostname().split('.', 1)[0]
+
+description_template = """Latest upstream release: %(latest_upstream)s
+
+Current version/release in %(repo_name)s: %(repo_version)s-%(repo_release)s
+
+URL: %(url)s
+
+
+Please consult the package updates policy before you
+issue an update to a stable branch:
+https://fedoraproject.org/wiki/Updates_Policy
+
+
+More information about the service that created this bug can be found at:
+
+%(explanation_url)s
+"""
+
+config = {
+    'hotness.bugzilla.enabled': True,
+
+    'hotness.bugzilla': {
+        #'user': None,
+        #'password': None,
+        'url': 'https://partner-bugzilla.redhat.com',
+        'product': 'Fedora',
+        'version': 'rawhide',
+        'keywords': 'FutureFeature,Triaged',
+        'bug_status': 'NEW',
+        'explanation_url': 'https://fedoraproject.org/wiki/Upstream_release_monitoring',
+        'short_desc_template': "%(name)s-%(latest_upstream)s is available",
+        'description_template': description_template,
+    },
+
+    'hotness.koji': {
+        'server': 'https://koji.fedoraproject.org/kojihub',
+        'weburl': 'http://koji.fedoraproject.org/koji',
+        'cert': os.path.expanduser('~/.fedora.cert'),
+        'ca_cert': os.path.expanduser('~/.fedora-server-ca.cert'),
+        'git_url': 'http://pkgs.fedoraproject.org/cgit/{package}.git',
+        'userstring': ('Fedora Release Monitoring '
+                       '<release-monitoring@fedoraproject.org>'),
+        'opts': {'scratch': True},
+        'priority': 30,
+        'target_tag': 'rawhide',
+    },
+
+    "hotness.cache": {
+        "backend": "dogpile.cache.dbm",
+        "expiration_time": 300,
+        "arguments": {
+            "filename": "/var/tmp/the-new-hotness-cache.dbm",
+        },
+    },
+}
+
