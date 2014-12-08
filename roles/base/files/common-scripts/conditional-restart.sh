09137e6de06249c78723bf163b745158182ade4e
Miroslav Suchý <msuchy@redhat.com>
2014-12-08 13:44:17 +0000

    remove old copr playbooks

--- roles/base/files/common-scripts/conditional-restart.sh
+++ roles/base/files/common-scripts/conditional-restart.sh
@@ -10,9 +10,13 @@ rpm -q $PACKAGE
 INSTALLED=$?
 
 if [ $INSTALLED -eq 0 ]; then
-    echo "Package $PACKAGE installed.  Attempting restart of $SERVICE."
-    /sbin/service $SERVICE restart
-    exit $?  # Exit with the /sbin/service status code
+    if chkconfig $PACKAGE; then
+        echo "Package $PACKAGE installed.  Attempting restart of $SERVICE."
+        /sbin/service $SERVICE restart
+        exit $?  # Exit with the /sbin/service status code
+    else
+        echo "Package $PACKAGE not enabled.  Skipping restart of $SERVICE."
+    fi
 fi
 
 # If the package wasn't installed, then pretend everything is fine.
