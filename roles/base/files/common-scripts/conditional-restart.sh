ada13bf8228b149892d18cb41a71efb4d6052aff
Miroslav Suchý <msuchy@redhat.com>
2014-12-08 13:46:23 +0000

    accidentally leaked to previous commit, reverting back

--- roles/base/files/common-scripts/conditional-restart.sh
+++ roles/base/files/common-scripts/conditional-restart.sh
@@ -10,13 +10,9 @@ rpm -q $PACKAGE
 INSTALLED=$?
 
 if [ $INSTALLED -eq 0 ]; then
-    if chkconfig $PACKAGE; then
-        echo "Package $PACKAGE installed.  Attempting restart of $SERVICE."
-        /sbin/service $SERVICE restart
-        exit $?  # Exit with the /sbin/service status code
-    else
-        echo "Package $PACKAGE not enabled.  Skipping restart of $SERVICE."
-    fi
+    echo "Package $PACKAGE installed.  Attempting restart of $SERVICE."
+    /sbin/service $SERVICE restart
+    exit $?  # Exit with the /sbin/service status code
 fi
 
 # If the package wasn't installed, then pretend everything is fine.
