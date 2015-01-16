03c3a225745db57a4b25e0d74e3152d3683664ed
Valentin Gologuzov <vgologuz@redhat.com>
2015-01-16 16:22:04 +0100

    [copr] [keygen] fixed typo .

--- roles/copr/keygen/files/backup_keyring.sh
+++ roles/copr/keygen/files/backup_keyring.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
 # used as root
-# root gpg keychain should have PUBLIC key with `user email` infra@fedoraporject.org
+# root gpg keychain should have PUBLIC key with `user email` infra@fedoraproject.org
 
 PATH_TO_KEYRING_DIR="/var/lib/copr-keygen"
 OUTPUT_FILE="/backup/copr_keygen_keyring.tar.gz.gpg"
 
-tar -cvzf - $1 | gpg2 --output $2 --encrypt --recipient infra@fedoraporject.org
+tar -cvzf - $1 | gpg2 --output $2 --encrypt --recipient infra@fedoraproject.org
