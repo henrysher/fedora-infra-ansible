86589714abc13443d7232424d454b81e96336cbb
Pierre-Yves Chibon <pingou@pingoured.fr>
2015-01-20 19:27:37 +0100

    Let the gen-acls user compile the configuration file

--- roles/distgit/files/genacls.sh
+++ roles/distgit/files/genacls.sh
@@ -7,7 +7,8 @@ cd $TEMPDIR
 # Only replace the acls if genacls completes successfully
 if /usr/local/bin/genacls.pkgdb > gitolite.conf ; then
     mv gitolite.conf /etc/gitolite/conf/
-    /usr/bin/gitolite compile
+    chown gen-acls:gen-acls -R /etc/gitolite/conf/
+    sudo -u gen-acls /usr/bin/gitolite compile
 fi
 cd /
 rm -rf $TEMPDIR
