b7febb9f6343f4170a7a3ab7a52b60f8388652cc
Valentin Gologuzov <vgologuz@redhat.com>
2015-01-21 22:10:39 +0100

    [copr] keygen, fixed backup script

--- roles/copr/keygen/files/backup_keyring.sh
+++ roles/copr/keygen/files/backup_keyring.sh
@@ -6,4 +6,4 @@
 PATH_TO_KEYRING_DIR="/var/lib/copr-keygen"
 OUTPUT_FILE="/backup/copr_keygen_keyring.tar.gz.gpg"
 
-tar -cvzf - $PATH_TO_KEYRING_DIR | gpg2 --output $OUTPUT_FILE --encrypt --recipient admin@fedoraproject.org
+tar -cvzf - $PATH_TO_KEYRING_DIR | gpg2 --output $OUTPUT_FILE --encrypt --recipient admin@fedoraproject.org --always-trust -v
