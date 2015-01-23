7902c6242519068d707f0576eef83e5db439be76
Kevin Fenzi <kevin@scrye.com>
2015-01-23 19:33:38 +0000

    Adjust for moving koji01/02 to production.

--- roles/keepalived/files/keepalived-notify.sh
+++ roles/keepalived/files/keepalived-notify.sh
@@ -7,11 +7,10 @@ STATE=$3
 # We are becoming master node
 #
 if [ $STATE == "MASTER" ]; then
-	# systemctl stop kojira
-	# rm -f /etc/cron.d/kojifix
-	# rm -f /etc/cron.d/koji-directory-cleanup
-	# rm -f /etc/cron.d/koji-gc
-	# rm -f /etc/cron.d/koji-prunesigs
+	systemctl stop kojira
+	rm -f /etc/cron.d/koji-directory-cleanup
+	rm -f /etc/cron.d/koji-gc
+	rm -f /etc/cron.d/koji-prunesigs
 	logger "just became keepalived master"
 
 fi
@@ -19,11 +18,10 @@ fi
 # We are becoming the backup node
 #
 if [ $STATE == "BACKUP" ]; then
-	# systemctl start kojira
-	#  /etc/cron.d/kojifix
-	# rm -f /etc/cron.d/koji-directory-cleanup
-	# rm -f /etc/cron.d/koji-gc
-	# rm -f /etc/cron.d/koji-prunesigs
+	systemctl start kojira
+        cp -a /usr/local/etc/koji-directory-cleanup /etc/cron.d/koji-directory-cleanup
+        cp -a /usr/local/etc/koji-directory-cleanup /etc/cron.d/koji-gc
+        cp -a /usr/local/etc/koji-directory-cleanup /etc/cron.d/koji-prunesigs
 	logger "just became keepalived backup"
 fi
 #
