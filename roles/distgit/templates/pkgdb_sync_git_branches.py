2c7598455b4d9a79d06fa11dc366d0b4ba8dcae6
Patrick Uiterwijk <puiterwijk@redhat.com>
2015-02-24 09:48:16 +0000

    Publish master creation

--- roles/distgit/templates/pkgdb_sync_git_branches.py
+++ roles/distgit/templates/pkgdb_sync_git_branches.py
@@ -190,6 +190,15 @@ def branch_package(pkgname, branches):
             os.path.join(GIT_FOLDER, '%s.git' % pkgname)):
         _invoke(SETUP_PACKAGE, [pkgname])
         branches.remove('master')  # SETUP_PACKAGE creates master
+        fedmsg.publish(
+            topic='branch',
+            modname='git',
+            msg=dict(
+                agent='pkgdb',
+                name=pkgname,
+                branch='master',
+            ),
+        )
 
     # Create all the required branches for the package
     # Use the translated branch name until pkgdb falls inline
