1b54f37dbddc12eb6099b3c1018b6c09d47e3f4f
Miroslav Suchý <msuchy@redhat.com>
2015-03-23 12:36:51 +0000

    add filters for openstack

---
+++ filter_plugins/openstack.py
@@ -0,0 +1,26 @@
+from ansible import errors, runner
+import json
+from novaclient.v3.client import Client
+import novaclient.exceptions;
+
+def flavor_id_to_name(host_vars, user, password, tenant, auth_url):
+    nt = Client(user, password, tenant, auth_url, service_type="compute")
+    try:
+        flavor = nt.flavors.get(host_vars)
+    except novaclient.exceptions.NotFound:
+        raise errors.AnsibleFilterError('There is no flavor of name {0}'.format(host_vars))
+    return flavor.name
+
+
+def flavor_name_to_id(host_vars, user, password, tenant, auth_url):
+    nt = Client(user, password, tenant, auth_url, service_type="compute")
+    for i in nt.flavors.list():
+        if i.name == host_vars:
+          return i.id
+    raise errors.AnsibleFilterError('There is no flavor of id {0}'.format(host_vars))
+
+class FilterModule (object):
+    def filters(self):
+        return {"flavor_id_to_name": flavor_id_to_name,
+            "flavor_name_to_id": flavor_name_to_id,
+        }
