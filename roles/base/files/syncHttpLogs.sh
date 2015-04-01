59488842f97099ab9d96bb5dbebb9ec0212c79d3
Kevin Fenzi <kevin@scrye.com>
2015-04-01 19:47:12 +0000

    Fix proxy11 http sync

--- roles/base/files/syncHttpLogs.sh
+++ roles/base/files/syncHttpLogs.sh
@@ -38,7 +38,7 @@ syncHttpLogs proxy07.vpn.fedoraproject.org
 syncHttpLogs proxy08.vpn.fedoraproject.org
 syncHttpLogs proxy09.vpn.fedoraproject.org
 syncHttpLogs proxy10.phx2.fedoraproject.org
-syncHttpLogs proxy11.phx2.fedoraproject.org
+syncHttpLogs proxy11.vpn.fedoraproject.org
 syncHttpLogs proxy01.stg.phx2.fedoraproject.org
 syncHttpLogs app01.stg.phx2.fedoraproject.org
 syncHttpLogs bapp02.phx2.fedoraproject.org
