18bccd797fe3c01dbeeac737ea57b27048d4cc92
Aurélien Bompard <aurelien@bompard.org>
2015-04-29 16:10:20 +0000

    Update the mailman role for Mailman 3

--- roles/mailman/files/clean-sessions.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-CONFFILE=/etc/mailman-migration.conf
-
-set -e 
-export PATH=$PATH:$(dirname $(realpath $0)) # make yamlget available
-
-CONFDIR=`yamlget confdir $CONFFILE`
-django-admin clearsessions --pythonpath $CONFDIR --settings settings
