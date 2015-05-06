16e008282488b82b198eb26c0b5094569114cb9b
Adrian Reber <adrian@lisas.de>
2015-05-06 09:02:31 +0000

    Distribute the hosts to be crawled by MM2 between the existing crawlers
    This commit adds two new scripts to the crawlers:
     * mm2_get-highest-active-host-id: this queries the MM database and returns
       the id of the active mirror with the highest id
     * run_crawler.sh: this calculates the right startid and stopid parameters
       depending on the number of available crawlers
    
    These scripts are integrated into the start of the crawler so that each
    crawler only crawls a subset of the existing mirrors. This distribution
    is not perfect as it pretends that the active mirrors are equally
    distributed under the total number of existing mirrors.

---
+++ roles/mirrormanager/crawler/files/run_crawler.sh
@@ -0,0 +1,29 @@
+#/bin/bash
+
+if [ $# -ne 1 ]; then
+        exit 0
+fi
+
+NUMBER_OF_CRAWLERS=$1
+HOST=`hostname -s`
+MAX_HOST=`/usr/local/bin/mm2_get-highest-active-host-id`
+
+# make MAX_HOST a multiple of NUMBER_OF_CRAWLERS
+let FIX=${MAX_HOST}%${NUMBER_OF_CRAWLERS}
+
+if [ "${FIX}" -ne "0" ]; then
+        let MAX_HOST=${MAX_HOST}+${NUMBER_OF_CRAWLERS}-${FIX}
+fi
+
+let PART=${MAX_HOST}/${NUMBER_OF_CRAWLERS}
+
+STARTID=0
+STOPID=${PART}
+
+for i in `seq 1 ${NUMBER_OF_CRAWLERS}`; do
+        if [ "${HOST}" == "mm-crawler0${i}" ]; then
+                echo "--startid=${STARTID} --stopid=${STOPID}"
+        fi
+        let STARTID=${STARTID}+${PART}
+        let STOPID=${STOPID}+${PART}
+done
