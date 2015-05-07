35296c746df13ea031a7408179c12e7e5a387dd6
Adrian Reber <adrian@lisas.de>
2015-05-07 10:04:36 +0000

    Only sync the files actually needed on the mirrorlist servers
    Also enable rsync compression like the old script did.

--- roles/mirrormanager/backend/templates/sync_pkl_to_mirrorlists.sh
+++ roles/mirrormanager/backend/templates/sync_pkl_to_mirrorlists.sh
@@ -3,6 +3,6 @@
 MIRRORLIST_SERVERS="{% for host in groups['mirrorlist2'] %} {{ host }} {% endfor %}"
 
 for s in ${MIRRORLIST_SERVERS}; do
-	rsync -a --delete-delay --delay-updates --delete /var/lib/mirrormanager/ ${s}:/var/lib/mirrormanager/
+	rsync -az --delete-delay --delay-updates --delete /var/lib/mirrormanager/{*pkl,*txt} ${s}:/var/lib/mirrormanager/
 	ssh $s 'kill -HUP $(cat /var/run/mirrormanager/mirrorlist_server.pid)'
 done
