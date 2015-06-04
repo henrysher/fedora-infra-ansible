9d31407e01bd878077ec9ddef857915352244971
Valentin Gologuzov <vgologuz@redhat.com>
2015-06-04 18:48:15 +0200

    [copr][keygen] silencing backup script

--- roles/copr/keygen/files/backup_keyring.sh
+++ roles/copr/keygen/files/backup_keyring.sh
@@ -6,6 +6,6 @@
 PATH_TO_KEYRING_DIR="/var/lib/copr-keygen"
 OUTPUT_FILE="/backup/copr_keygen_keyring.tar.gz.gpg"
 
-tar -cvzf - $PATH_TO_KEYRING_DIR |
-  gpg2 --output $OUTPUT_FILE.tmp --encrypt --recipient admin@fedoraproject.org --always-trust -v &&
+tar -czPf - $PATH_TO_KEYRING_DIR |
+  gpg2 --output $OUTPUT_FILE.tmp --encrypt --recipient admin@fedoraproject.org --always-trust &&
   mv $OUTPUT_FILE.tmp $OUTPUT_FILE
