d609f07430eee3df6cf5e012399443a9a9f8d91f
Valentin Gologuzov <vgologuz@redhat.com>
2015-06-11 17:09:38 +0200

    [dopr] fix config

--- roles/dopr/templates/cdic.py
+++ roles/dopr/templates/cdic.py
@@ -9,6 +9,7 @@ HUB_PROJECT_URL_TEMPLATE = 'http://registry.hub.docker.com/u/cdictest/{repo_name
 
 GITHUB_TOKEN = '{{ dopr_testing_github_token }}'
 GITHUB_USER = '{{ dopr_testing_github_username }}'
+GITHUB_PASSWORD = '{{ dopr_testing_github_password }}'
 GITHUB_API_ROOT = 'https://api.github.com'
 
 SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://cdic:{{ dopr_db_passwd }}@localhost/cdicdb'
