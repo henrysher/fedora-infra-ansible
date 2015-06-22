49e9f30aa4f3ce9bf5971eeccb8215669ab13191
Adrian Reber <adrian@lisas.de>
2015-06-22 21:07:15 +0000

    Only sync current logs and not the old ones.

--- roles/mirrormanager/frontend2/templates/sync-crawler-logs.sh
+++ roles/mirrormanager/frontend2/templates/sync-crawler-logs.sh
@@ -3,5 +3,5 @@
 CRAWLERS="{% for host in groups['mm-crawler'] %} {{ host }} {% endfor %}"
 
 for i in ${CRAWLERS}; do
-	rsync -aq ${i}::crawler/ /var/log/mirrormanager/crawler/
+	rsync -aq ${i}::crawler/*log /var/log/mirrormanager/crawler/
 done
