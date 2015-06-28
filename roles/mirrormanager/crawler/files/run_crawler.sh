0b76e6721aeb203171d382043bce8fea2f85bba1
Adrian Reber <adrian@lisas.de>
2015-06-28 10:24:56 +0000

    Use the new script to better distribute the load between the crawlers.

--- roles/mirrormanager/crawler/files/run_crawler.sh
+++ roles/mirrormanager/crawler/files/run_crawler.sh
@@ -6,27 +6,11 @@ fi
 
 NUMBER_OF_CRAWLERS=$1
 HOST=`hostname -s`
-MAX_HOST=`/usr/local/bin/mm2_get-highest-active-host-id`
+START_STOP="/usr/local/bin/mm2_get-mirrors-to-crawl"
 
-# make MAX_HOST a multiple of NUMBER_OF_CRAWLERS
-let FIX=${MAX_HOST}%${NUMBER_OF_CRAWLERS}
-
-if [ "${FIX}" -ne "0" ]; then
-        let MAX_HOST=${MAX_HOST}+${NUMBER_OF_CRAWLERS}-${FIX}
-fi
-
-let PART=${MAX_HOST}/${NUMBER_OF_CRAWLERS}
-
-STARTID=0
-STOPID=${PART}
 
 for i in `seq 1 ${NUMBER_OF_CRAWLERS}`; do
         if [ "${HOST}" == "mm-crawler0${i}" ]; then
-                echo "--startid=${STARTID} --stopid=${STOPID}"
+		${START_STOP} -f ${i}:${NUMBER_OF_CRAWLERS}
         fi
-        let STARTID=${STARTID}+${PART}
-        let STOPID=${STOPID}+${PART}
-	if [ "${STOPID}" -eq "${MAX_HOST}" ]; then
-		let STOPID=${STOPID}+1
-	fi
 done
