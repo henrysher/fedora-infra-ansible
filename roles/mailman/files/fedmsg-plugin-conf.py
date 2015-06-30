13c0fc577e78697333722dae7216bb7bdfd50a34
Ralph Bean <rbean@redhat.com>
2015-06-30 14:22:17 +0000

    Fix syntax error.

--- roles/mailman/files/fedmsg-plugin-conf.py
+++ roles/mailman/files/fedmsg-plugin-conf.py
@@ -1,6 +1,6 @@
 config = {
     # These are mailing lists that we don't publish to fedmsg (because spam)
-    'mailman.excluded_lists' = [
+    'mailman.excluded_lists': [
         'scm-commits',
         #'i18n-bugs',# disable this eventually
         #'fonts-bugs', # disable this eventually
