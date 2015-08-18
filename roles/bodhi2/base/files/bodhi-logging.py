8ed37dd8ab4b2ab6755116acf608e7e8e38ecb9f
Ralph Bean <rbean@redhat.com>
2015-08-18 21:47:03 +0000

    Copy a logging config file for the fedmsg-hub (bodhi2).

---
+++ roles/bodhi2/base/files/bodhi-logging.py
@@ -0,0 +1,23 @@
+# This should get merged smartly with the site-wide fedmsg.d/logging.py we have
+# installed everywhere.
+config = dict(
+    logging=dict(
+        loggers=dict(
+            bodhi={
+                "level": "DEBUG",
+                "propagate": False,
+                "handlers": ["console", "mailer"],
+            },
+            bodhi={
+                "level": "WARN",
+                "propagate": False,
+                "handlers": ["console", "mailer"],
+            },
+            root={
+                "level": "INFO",
+                "propagate": False,
+                "handlers": ["console", "mailer"],
+            },
+        ),
+    ),
+)
