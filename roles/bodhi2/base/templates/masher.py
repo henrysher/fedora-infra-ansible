68e499afdd0f60ae90517ca64ce34feca81cd363
Ralph Bean <rbean@redhat.com>
2015-09-03 19:47:01 +0000

    Set config values for the bodhi backend processes.

--- roles/bodhi2/base/templates/masher.py
+++ roles/bodhi2/base/templates/masher.py
@@ -7,11 +7,7 @@ suffix = 'phx2.fedoraproject.org'
 config = dict(
     # Note, the masher runs on bodhi-backend01, while other consumers will run
     # on bodhi-backend02.
-    {% if inventory_hostname.startswith('bodhi-backend01') %}
-    masher=True,
-    {% else %}
-    masher=False,
-    {% endif %}
+    masher={{bodhi_masher_enabled}},
     masher_topic='bodhi.masher.start',
     releng_fedmsg_certname='shell-bodhi-backend01.%s' % suffix,
 )
--- roles/bodhi2/base/templates/updates_handler.py
+++ roles/bodhi2/base/templates/updates_handler.py
@@ -1,9 +1,5 @@
 config = dict(
     # Note, the masher runs on bodhi-backend01, but this
     # runs on bodhi-backend02 (separation of concerns).
-    {% if inventory_hostname.startswith('bodhi-backend02') %}
-    updates_handler=True,
-    {% else %}
-    updates_handler=False,
-    {% endif %}
+    updates_handler={{bodhi_updates_handler_enabled}},
 )
