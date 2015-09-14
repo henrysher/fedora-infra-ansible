19a757bdcd6b1009afa7fe0f17fc657e7e5527d9
Valentin Gologuzov <vgologuz@redhat.com>
2015-09-14 20:04:08 +0200

    [copr][dist-git] adding backup

--- roles/copr/dist_git/files/create_backup.sh
+++ roles/copr/dist_git/files/create_backup.sh
@@ -2,8 +2,10 @@
 
 systemctl stop copr-dist-git
 
+
 cd /var/lib/dist-git/
+mkdir -p backup
 tar --selinux --acls --xattrs -czf tmp.backup.tar.gz /var/lib/dist-git/cache /var/lib/dist-git/git /var/lib/dist-git/gitolite /var/lib/dist-git/web
-mv -f tmp.backup.tar.gz backup.tar.gz
+mv -f tmp.backup.tar.gz backup/copr_dist_git.tar.gz
 
 systemctl start copr-dist-git
