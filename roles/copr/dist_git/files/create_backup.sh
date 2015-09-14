e543624f148cd2a514b5b90ee41dff8d50625df0
Valentin Gologuzov <vgologuz@redhat.com>
2015-09-14 19:42:16 +0200

    [copr] typo

--- roles/copr/dist_git/files/create_backup.sh
+++ roles/copr/dist_git/files/create_backup.sh
@@ -4,6 +4,6 @@ systemctl stop copr-dist-git
 
 cd /var/lib/dist-git/
 tar --selinux --acls --xattrs -czf tmp.backup.tar.gz /var/lib/dist-git/cache /var/lib/dist-git/git /var/lib/dist-git/gitolite /var/lib/dist-git/web
-mv -f tmp.backup.tar.gz tmp.backup.tar.gz
+mv -f tmp.backup.tar.gz backup.tar.gz
 
 systemctl start copr-dist-git
