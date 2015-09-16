c0f68910f32dfb9e792fd0e735de7f09ffd59ae5
Ralph Bean <rbean@redhat.com>
2015-09-16 17:51:11 +0000

    Add manual trigger script for fedimg.
    Better to keep it here than tucked away in /home/fedora/oddshocks/

---
+++ roles/fedimg/files/trigger_upload.py
@@ -0,0 +1,30 @@
+#!/bin/env python
+# -*- coding: utf8 -*-
+""" Triggers an upload process with the specified raw.xz URL. """
+
+import logging
+import logging.config
+import multiprocessing.pool
+import sys
+
+import fedmsg
+import fedmsg.config
+
+import fedimg
+import fedimg.services
+from fedimg.services.ec2 import EC2Service, EC2ServiceException
+import fedimg.uploader
+from fedimg.util import virt_types_from_url
+
+if len(sys.argv) != 2:
+    print 'Usage: trigger_upload.py <rawxz_image_url>'
+    sys.exit(1)
+
+logging.config.dictConfig(fedmsg.config.load_config()['logging'])
+log = logging.getLogger('fedmsg')
+
+upload_pool = multiprocessing.pool.ThreadPool(processes=4)
+
+url = sys.argv[1]
+
+fedimg.uploader.upload(upload_pool, [url])
