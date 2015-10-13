dcfe1d6a6403009e5d4502df1ccac6ace00d8673
Ralph Bean <rbean@redhat.com>
2015-10-13 17:03:15 +0000

    Fix syntax error in resultsdb fedmsg config.

--- roles/taskotron/resultsdb-fedmsg/files/resultsdb.py
+++ roles/taskotron/resultsdb-fedmsg/files/resultsdb.py
@@ -4,5 +4,5 @@ config = {
     # in /etc/fedmsg.d/relay.py instead of binding to ports and waiting for
     # listeners.  We need this so that we can push our messages on to the
     # fedora-infra proper bus from over in the QA network.
-    active=True,
+    'active': True,
 }
