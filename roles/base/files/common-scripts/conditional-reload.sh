17d96e5da9f58b44866595a2a052f041aa1f98ad
Kevin Fenzi <kevin@scrye.com>
2015-11-13 23:49:38 +0000

    Add a conditional-reload script and use it so we can not fail on hosts with no httpd installed.

---
+++ roles/base/files/common-scripts/conditional-reload.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+# reload SERVICE only if PACKAGE is installed.
+# We use this throughout handlers/restart_services.yml
+
+SERVICE=$1
+PACKAGE=$2
+
+rpm -q $PACKAGE
+
+INSTALLED=$?
+
+if [ $INSTALLED -eq 0 ]; then
+    echo "Package $PACKAGE installed.  Attempting reload of $SERVICE."
+    /sbin/service $SERVICE reload
+    exit $?  # Exit with the /sbin/service status code
+fi
+
+# If the package wasn't installed, then pretend everything is fine.
+echo "Package $PACKAGE not installed.  Skipping reload of $SERVICE."
+exit 0
