29d8fdd750e9b4a9a3afb365745c2a4984f8ea0a
Kevin Fenzi <kevin@scrye.com>
2015-11-20 23:28:56 +0000

    Commit revised spec checkout seed modifications from ticket 4941

--- roles/git/make_checkout_seed/files/make-git-checkout-seed.sh
+++ roles/git/make_checkout_seed/files/make-git-checkout-seed.sh
@@ -3,6 +3,8 @@
 # mkdir /srv/git_seed
 # cron job to invoke file daily
 # Need to setup OUTPUT_DIR to be served by apache
+# modified by doteast
+# modified by alda
 
 # Where the git repos live.  These are bare repos
 ORIGIN_DIR=/srv/git/rpms
@@ -12,12 +14,16 @@ WORK_DIR=/srv/git_seed
 # Subdirectory makes cleanup easier
 SEED_DIR=$WORK_DIR/git-checkout
 
+# subdirectory to collect rpm speciles
+SPEC_DIR=$WORK_DIR/rpm-specs
+
 # Where to store the seed tarball for download
 OUTPUT_DIR=/srv/cache/lookaside/
 
 # Instead of starting fresh each time, we'll try to use git pull to keep things synced
 #rm -rf $WORK_DIR/*
 mkdir -p $SEED_DIR
+mkdir -p $SPEC_DIR
 
 # Give people an indication of when this checkout was last synced
 TIMESTAMP=`date --rfc-3339=seconds`
@@ -33,15 +39,25 @@ for repo in $ORIGIN_DIR/*.git ; do
     git pull --all &> /dev/null
     sed -i "s@url = .*@url = git://pkgs.fedoraproject.org/$(basename $repo .git)@" $working_tree/.git/config 
     popd &>/dev/null
+    if [ -e $working_tree/$(basename $repo .git).spec ]; then
+      cp $working_tree/$(basename $repo .git).spec $SPEC_DIR/
+    fi
   else
     pushd $SEED_DIR &>/dev/null
     git clone $repo &> /dev/null
     popd &>/dev/null
-    sed -i "s@url = .*@url = git://pkgs.fedoraproject.org/$(basename $repo .git)@" $working_tree/.git/config 
+    sed -i "s@url = .*@url = git://pkgs.fedoraproject.org/$(basename $repo .git)@" $working_tree/.git/config
+    if [ -e $working_tree/$(basename $repo .git).spec ]; then
+      cp $working_tree/$(basename $repo .git).spec $SPEC_DIR/
+    fi
     fi
 done
 
 tar -cf - -C$WORK_DIR $(basename $SEED_DIR)|xz -2 > $OUTPUT_DIR/.git-seed-$DATE.tar.xz
+tar -cf - -C$WORK_DIR $(basename $SPEC_DIR)|xz -2 > $OUTPUT_DIR/.rpm-specs-$DATE.tar.xz
 rm $OUTPUT_DIR/git-seed*tar.xz
+rm $OUTPUT_DIR/rpm-specs*tar.xz
 mv $OUTPUT_DIR/.git-seed-$DATE.tar.xz $OUTPUT_DIR/git-seed-$DATE.tar.xz
+mv $OUTPUT_DIR/.rpm-specs-$DATE.tar.xz $OUTPUT_DIR/rpm-specs-$DATE.tar.xz
 ln -s git-seed-$DATE.tar.xz $OUTPUT_DIR/git-seed-latest.tar.xz
+ln -s rpm-specs-$DATE.tar.xz $OUTPUT_DIR/rpm-specs-latest.tar.xz
