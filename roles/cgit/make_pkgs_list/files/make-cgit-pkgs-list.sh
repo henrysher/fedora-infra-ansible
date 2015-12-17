617beaac5fb665941a8a1df9bc4488c463e3f56c
Pierre-Yves Chibon <pingou@pingoured.fr>
2015-12-17 16:34:17 +0100

    Update make-cgit-pkgs-list.sh for namespacing

--- roles/cgit/make_pkgs_list/files/make-cgit-pkgs-list.sh
+++ roles/cgit/make_pkgs_list/files/make-cgit-pkgs-list.sh
@@ -7,8 +7,15 @@
 #
 newfile=`mktemp`
 
-cd /srv/git/rpms
 ls > $newfile
+cd /srv/git/repositories/
+for d in `ls`; do
+  if [ ! -L $d ]; then
+    for f in `ls $d`; do
+     echo $d/$f >> $newfile;
+    done;
+  fi;
+done;
 mv -Z $newfile /srv/git/pkgs-git-repos-list
 chown apache:apache /srv/git/pkgs-git-repos-list
 chmod 644 /srv/git/pkgs-git-repos-list
