8d70839c020e2709dd96da54dcce56ab07f43a43
Ralph Bean <rbean@redhat.com>
2015-12-17 16:13:12 +0000

    A stab at fixing the make-cgit list script.

--- roles/cgit/make_pkgs_list/files/make-cgit-pkgs-list.sh
+++ roles/cgit/make_pkgs_list/files/make-cgit-pkgs-list.sh
@@ -6,16 +6,17 @@
 # Looking for git repos. 
 #
 newfile=`mktemp`
+target=/srv/git/repositories
 
-ls > $newfile
-cd /srv/git/repositories/
-for d in `ls`; do
-  if [ ! -L $d ] && [ -d $d ]; then
-    for f in `ls $d`; do
-     echo $d/$f >> $newfile;
+ls $target > $newfile
+for d in `ls $target`; do
+  if [ ! -L $target/$d ] && [ -d $target/$d ]; then
+    for f in `ls $target/$d/`; do
+     echo "$d/$f" >> $newfile;
     done;
   fi;
 done;
+
 mv -Z $newfile /srv/git/pkgs-git-repos-list
 chown apache:apache /srv/git/pkgs-git-repos-list
 chmod 644 /srv/git/pkgs-git-repos-list
