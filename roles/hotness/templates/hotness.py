a8a2066c38cf0165d638f1e86ea8e3d85e72a046
Ralph Bean <rbean@redhat.com>
2016-03-14 20:38:44 +0000

    Add link to anitya project in hotness bug template.

--- roles/hotness/templates/hotness.py
+++ roles/hotness/templates/hotness.py
@@ -11,6 +11,8 @@ Please consult the package updates policy before you issue an update to a stable
 More information about the service that created this bug can be found at: %(explanation_url)s
 
 Please keep in mind that with any upstream change, there may also be packaging changes that need to be made. Specifically, please remember that it is your responsibility to review the new version to ensure that the licensing is still correct and that no non-free or legally problematic items have been added upstream.
+
+Based on the information from anitya:  https://release-monitoring.org/project/%(projectid)s/
 """
 
 config = {
