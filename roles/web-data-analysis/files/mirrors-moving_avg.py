7accacc403ea999caacd4c851533efffd60b135c
Stephen Smoogen <smooge@redhat.com>
2016-05-07 14:15:18 +0000

    python is hard. lets go shopping

--- roles/web-data-analysis/files/mirrors-moving_avg.py
+++ roles/web-data-analysis/files/mirrors-moving_avg.py
@@ -1,7 +1,29 @@
-# Moving Average
+#!/usr/bin/python
+
+# This file is part of Fedora Project Infrastructure Ansible
+# Repository.
+#
+# Fedora Project Infrastructure Ansible Repository is free software:
+# you can redistribute it and/or modify it under the terms of the GNU
+# General Public License as published by the Free Software Foundation,
+# either version 3 of the License, or (at your option) any later
+# version.
+#
+# Fedora Project Infrastructure Ansible Repository is distributed in
+# the hope that it will be useful, but WITHOUT ANY WARRANTY; without
+# even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+# PARTICULAR PURPOSE.  See the GNU General Public License for more
+# details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Fedora Project Infrastructure Ansible Repository.  If
+# not, see <http://www.gnu.org/licenses/>.
+
+# This is a complete horrible hack to get something done. Patches are
+# really welcome.
 
 import pandas
-import matplotlib.pyplot as plt
+#import matplotlib.pyplot as plt
 import math
 
 rolling = 7
