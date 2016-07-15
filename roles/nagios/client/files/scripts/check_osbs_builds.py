6685841b7c150ba9e3371294a9e40a2ba18afce1
Patrick Uiterwijk <puiterwijk@redhat.com>
2016-07-15 19:25:28 +0000

    Fix the osbs nagios script
    Signed-off-by: Patrick Uiterwijk <puiterwijk@redhat.com>

--- roles/nagios/client/files/scripts/check_osbs_builds.py
+++ roles/nagios/client/files/scripts/check_osbs_builds.py
@@ -6,14 +6,18 @@ import sys
 sp = subprocess.Popen(
     ["osbs", "list-builds"],
     stdout=subprocess.PIPE,
-    stderr=subprocess.PIPE
+    stderr=subprocess.PIPE,
+    stdin=subprocess.PIPE
 )
 sp_out, sp_err = sp.communicate()
+sp_err = sp_err.split('\n')
 
-if sp_err.split()[0] == 'BUILD':
+if 'not attached to terminal' in sp_err[0]:
+    sp_err = sp_err[1:]
+
+if sp_err[0].split()[0] == 'BUILD':
     print "OK: OSBS is responsive to 'osbs list-builds'"
     sys.exit(0)
 else:
     print "CRITICAL: OSBS UNRESPONSIVE"
     sys.exit(2)
-
