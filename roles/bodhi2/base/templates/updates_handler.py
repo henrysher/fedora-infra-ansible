51b123b57ce03b3e8d326fa3551959730e770bd4
Kevin Fenzi <kevin@scrye.com>
2016-09-06 16:22:25 +0000

    Move sync jobs over to bodhi-backend03 from bodhi-backend01. Disable bodhi-backend01 from nagios so we can remove it.

--- roles/bodhi2/base/templates/updates_handler.py
+++ roles/bodhi2/base/templates/updates_handler.py
@@ -1,5 +1,5 @@
 config = dict(
-    # Note, the masher runs on bodhi-backend01, but this
+    # Note, the masher runs on bodhi-backend03, but this
     # runs on bodhi-backend02 (separation of concerns).
     updates_handler={{bodhi_updates_handler_enabled}},
 )
