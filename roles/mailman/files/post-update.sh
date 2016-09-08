3feaacf3ebc2a5146fbb99e77d77ff2f8fe72949
Aurélien Bompard <aurelien@bompard.org>
2016-09-08 06:38:26 +0000

    Mailman: fix initial data

--- roles/mailman/files/post-update.sh
+++ roles/mailman/files/post-update.sh
@@ -10,9 +10,13 @@ BASEDIR=`yamlget basedir $CONFFILE`
 CONFDIR=`yamlget confdir $CONFFILE`
 INDEXDIR=$BASEDIR/fulltext_index
 
+echo "static files"
 django-admin collectstatic --clear --noinput --verbosity 0 --pythonpath $CONFDIR --settings settings
 django-admin compress --pythonpath $CONFDIR --settings settings
+echo "db migration"
 django-admin migrate --pythonpath $CONFDIR --settings settings_admin --noinput
+echo "load initial data"
+django-admin loaddata $CONFDIR/initial-data.json --pythonpath $CONFDIR --settings settings
 mkdir -p $INDEXDIR
 chown apache:apache -R $INDEXDIR
 
@@ -21,9 +25,11 @@ sleep $[ ( $RANDOM % 10 )  + 1 ]s # avoid simultaneous lockups on parallel serve
 $BASEDIR/bin/pg-give-rights.py > /dev/null
 
 # SELinux contexts
+echo "SELinux contexts"
 restorecon -r "$BASEDIR"
 
 # Run unit tests
+echo "unit tests"
 django-admin test --pythonpath $CONFDIR --settings settings_test hyperkitty postorius
 
 # Reload Apache to flush the python cache
