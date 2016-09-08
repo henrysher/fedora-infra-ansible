7f27054e609faa2032b038bb6a375d6acae3a9f5
Aurélien Bompard <aurelien@bompard.org>
2016-09-08 06:25:46 +0000

    Big Mailman update

--- roles/mailman/files/urls.py
+++ roles/mailman/files/urls.py
@@ -1,27 +1,18 @@
 # -*- coding: utf-8 -*-
 
-import hyperkitty
-import postorius
-
-from django.conf.urls import patterns, include, url
-from django.conf import settings
-
-# Uncomment the next two lines to enable the admin:
+from django.conf.urls import include, url
 from django.contrib import admin
-admin.autodiscover()
 
 from django.core.urlresolvers import reverse_lazy
 from django.views.generic import RedirectView
 
-urlpatterns = patterns('',
+urlpatterns = [
     url(r'^$', RedirectView.as_view(
         url=reverse_lazy('hyperkitty.views.index.index'),
         permanent=True)),
-    #url(r'^$', 'postorius.views.list_index'),
     url(r'^admin/', include('postorius.urls')),
     url(r'^archives/', include('hyperkitty.urls')),
-    # Social Auth
-    url(r'', include('social.apps.django_app.urls', namespace='social')),
-    # BrowserID
-    url(r'', include('django_browserid.urls')),
-)
+    url(r'', include('django_mailman3.urls')),
+    url(r'^accounts/', include('allauth.urls')),
+    url(r'^django-admin/', include(admin.site.urls)),
+]
