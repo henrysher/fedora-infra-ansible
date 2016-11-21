c85d3957195e50a28153380e925f402c924152e6
Ricky Elrod <codeblock@fedoraproject.org>
2016-11-21 14:00:27 +0000

    prepare modernpaste01.phx2
    Signed-off-by: Ricky Elrod <codeblock@fedoraproject.org>

--- roles/modernpaste/templates/flask_config.py
+++ roles/modernpaste/templates/flask_config.py
@@ -1,7 +1,12 @@
 import config
 import constants
 
+{% if env == 'staging' %}
 SQLALCHEMY_DATABASE_URI = 'postgres://{{modernpaste_stg_db_user}}:{{modernpaste_stg_db_password}}@db01/modernpaste'
+{% else %}
+SQLALCHEMY_DATABASE_URI = 'postgres://{{modernpaste_db_user}}:{{modernpaste_db_password}}@db01/modernpaste'
+{% endif %}
+
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 # Flask session secret key
