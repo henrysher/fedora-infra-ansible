bff96ef88ee6fd5fe0941ec3dd12dc397165efc3
Patrick Uiterwijk <puiterwijk@redhat.com>
2016-11-28 10:54:55 +0000

    Move IPA setup to ansible
    Signed-off-by: Patrick Uiterwijk <puiterwijk@redhat.com>

--- roles/ipa/server/files/configure-ipa.sh
+++ roles/ipa/server/files/configure-ipa.sh
@@ -15,12 +15,3 @@ do
     echo "Removing $line"
     ipa selfservice-del "$line"
 done
-
-# Create fas_sync user
-ipa user-add fas_sync --first=FAS --last=Sync
-
-# Allow sync user to create and edit users
-ipa group-add-member admins --users=fas_sync
-
-# Disable password expiration
-ipa pwpolicy-mod global_policy --maxlife=0 --minlife=0 --history=0 --minclasses=0 --minlength=0 --maxfail=0
