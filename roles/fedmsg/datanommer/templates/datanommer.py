6b738addbbaac8e5d87b47a3233d29a1178f7456
Patrick Uiterwijk <puiterwijk@redhat.com>
2016-11-30 10:30:59 +0000

    In staging, datanommer uses db01.stg
    Signed-off-by: Patrick Uiterwijk <puiterwijk@redhat.com>

--- roles/fedmsg/datanommer/templates/datanommer.py
+++ roles/fedmsg/datanommer/templates/datanommer.py
@@ -1,5 +1,9 @@
 # Configuration for the datanommer consumer.  A plugin for the fedmsg-hub process.
 config = {
     'datanommer.enabled': True,
+{% if env == "production" %}
     'datanommer.sqlalchemy.url': 'postgresql://{{ datanommerDBUser }}:{{ datanommerDBPassword }}@db-datanommer02/datanommer',
+{% else %}
+    'datanommer.sqlalchemy.url': 'postgresql://{{ datanommerDBUser }}:{{ datanommerDBPassword }}@db01.stg/datanommer',
+{% endif %}
 }
