a14e7b538aeec7bbc4471bce8daac433dbfd9285
Pierre-Yves Chibon <pingou@pingoured.fr>
2016-12-12 10:49:13 +0100

    Update summershum's config to point to src.fp.o

--- roles/summershum/templates/summershum.py
+++ roles/summershum/templates/summershum.py
@@ -2,7 +2,7 @@ config = {
     # This is just a key to tell the fedmsg-hub to initialize us.
     'summershum.enabled': True,
     'summershum.sqlalchemy.url': 'postgresql://{{summershum_db_user}}:{{summershum_db_password}}@db-summershum/summershum',
-    'summershum.lookaside': 'http://pkgs.fedoraproject.org/lookaside/pkgs/',
+    'summershum.lookaside': 'https://src.fedoraproject.org/lookaside/pkgs/',
     'summershum.datagrepper': 'https://apps.fedoraproject.org/datagrepper/',
 
     'logging': {
