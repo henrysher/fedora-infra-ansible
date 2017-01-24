dade4ed1af22a0a0d15e671610994a6471de9f53
Jakub Kadlčík <frostyx@email.cz>
2017-01-24 12:16:44 +0100

    Remove commented code

--- roles/copr/mbs/files/config.py
+++ roles/copr/mbs/files/config.py
@@ -22,23 +22,3 @@ class BaseConfiguration(base.BaseConfiguration):
 
 class TestConfiguration(base.TestConfiguration):
     pass
-
-
-
-
-
-
-# import sys
-# sys.path.insert(1, '/etc/module-build-service')
-#
-# from base_config import confdir, dbdir, \
-#                         BaseConfiguration, \
-#                         DevConfiguration, \
-#                         TestConfiguration
-#
-#
-# class ProdConfiguration(BaseConfiguration):
-#     SYSTEM = 'copr'
-#     FAS_USERNAME = 'someuser'
-#     FAS_PASSWORD = 'secretpassword'
-#     OIDC_CLIENT_SECRETS = '/etc/module-build-service/client_secrets.json'
