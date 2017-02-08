5d963eb31ba2195b93b29bae30cd0e6fd2b3d957
Kevin Fenzi <kevin@scrye.com>
2017-02-08 20:46:23 +0000

    add proxy02 to get pkl synced from mm-backend01

--- roles/mirrormanager/backend/templates/sync_pkl_to_mirrorlists.sh
+++ roles/mirrormanager/backend/templates/sync_pkl_to_mirrorlists.sh
@@ -6,3 +6,11 @@ for s in ${MIRRORLIST_SERVERS}; do
 	rsync -az --delete-delay --delay-updates --delete /var/lib/mirrormanager/{*pkl,*txt} ${s}:/var/lib/mirrormanager/
 	ssh $s 'kill -HUP $(cat /var/run/mirrormanager/mirrorlist_server.pid)'
 done
+
+# sync also to new mirrorlist containers on proxies
+
+MIRRORLIST_PROXY="proxy02.fedoraproject.org"
+
+for s in ${MIRRORLIST_PROXY}; do
+	rsync -az --delete-delay --delay-updates --delete /var/lib/mirrormanager/{*pkl,*txt} ${s}:/srv/mirrorlist/data/mirrorlist1/
+done
