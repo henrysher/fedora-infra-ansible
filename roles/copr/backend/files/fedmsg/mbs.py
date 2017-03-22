19fd3e6e6ed0276752e7433ee5ab9f7bc698d1a8
Jakub Kadlčík <frostyx@email.cz>
2017-03-22 20:23:47 +0100

    Run fedmsg-relay on copr-be-dev

---
+++ roles/copr/backend/files/fedmsg/mbs.py
@@ -0,0 +1,23 @@
+config = dict(
+
+    sign_messages=False,
+    validate_signatures=False,
+
+    endpoints={
+        # This is the output side of the relay to which all other
+        # services can listen.
+        "relay_outbound": [
+            "tcp://172.25.32.160:4001",
+        ],
+    },
+
+    # This is the address of an active->passive relay.  It is used for the
+    # fedmsg-logger command which requires another service with a stable
+    # listening address for it to send messages to.
+    # It is also used by the git-hook, for the same reason.
+    # It is also used by the mediawiki php plugin which, due to the oddities of
+    # php, can't maintain a single passive-bind endpoint of it's own.
+    relay_inbound=[
+        "tcp://172.25.32.160:2003",
+    ],
+)
