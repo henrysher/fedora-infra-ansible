0a1ed3134c43bc9f619b83423321379223659cb3
Jakub Kadlčík <frostyx@email.cz>
2017-04-11 13:54:54 +0200

    Properly configure our MBS instance

--- roles/copr/mbs/templates/config.py
+++ roles/copr/mbs/templates/config.py
@@ -7,9 +7,31 @@ from base_config import confdir, dbdir
 
 class ProdConfiguration(base.ProdConfiguration):
     SYSTEM = 'copr'
-    REQUIRE_PACKAGER = False
     SECRET_KEY = '{{ copr_mbs_secret_key }}'
+
+    YAML_SUBMIT_ALLOWED = True
+
+    # Only copr-frontend is allowed to communicate with this mbs instance
+    # Therefore we don't require it to authenicate first, we trust it
     NO_AUTH = True
+
+    # Use production instances of PDC and Koji
+    KOJI_REPOSITORY_URL = 'https://kojipkgs.fedoraproject.org/repos'
+    PDC_URL = 'http://pdc.fedoraproject.org/rest_api/v1'
+
+    # When building from scmurl,
+    # only such URLs that starts with some of these are allowed
+    # Currently it is not possible to turn this feature off
+    # and allow everything, so let's allow at least something
+    SCMURLS = ["git://pkgs.stg.fedoraproject.org/modules/",
+               "git://pkgs.fedoraproject.org/modules/"]
+
+{% if env == 'staging' %}
+    MESSAGING_TOPIC_PREFIX = ['org.fedoraproject.dev', 'org.fedoraproject.stg']
+{% else %}
+    MESSAGING_TOPIC_PREFIX = ['org.fedoraproject.prod']
+{% endif %}
+
 
 
 class DevConfiguration(base.DevConfiguration):
