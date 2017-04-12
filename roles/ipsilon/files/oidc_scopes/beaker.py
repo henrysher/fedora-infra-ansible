e16ddd4d539aed25d2b38d4e49e0f3f4eeb5c9a4
Ralph Bean <rbean@redhat.com>
2017-04-12 15:06:43 +0000

    Beaker openidc scope declaration.

---
+++ roles/ipsilon/files/oidc_scopes/beaker.py
@@ -0,0 +1,14 @@
+from __future__ import absolute_import
+
+from ipsilon.providers.openidc.plugins.common import OpenidCExtensionBase
+
+
+class OpenidCExtension(OpenidCExtensionBase):
+    name = 'beaker'
+    display_name = 'Beaker Jobs'
+    scopes = {
+        'https://beaker-project.org/oidc/scope': {
+            'display_name': 'Full CRUD permissions to your beaker account',
+            'claims': [],
+        },
+    }
