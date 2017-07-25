7180f1669ad7fe3a9e76ae24d7885dc675151e3a
Pierre-Yves Chibon <pingou@pingoured.fr>
2017-07-25 15:20:38 +0200

    Move fedmsg-genacls-config.py to a template and configure it for the new world

---
+++ roles/distgit/templates/fedmsg-genacls-config.py
@@ -0,0 +1,12 @@
+config = {
+    'genacls.consumer.enabled': True,
+    'genacls.consumer.delay': 5, # 5 seconds
+
+    # New world
+    'gitoliteprefix.consumer.enabled': True,
+    'gitoliteprefix.consumer.delay': 5, # 5 seconds
+    'gitoliteprefix.consumer.filename': '/var/tmp/gitolite-prefix.txt',
+    'gitoliteprefix.consumer.fasurl': 'https://admin.fedoraproject.org/accounts',
+    'gitoliteprefix.consumer.username': "fasreadonly",
+    'gitoliteprefix.consumer.password': {{ fasReadOnlyPassword }},
+}
