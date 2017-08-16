06df919c1b6f4c164714ab8412ce8bde23fc5645
Ralph Bean <rbean@redhat.com>
2017-08-16 17:49:37 +0000

    Run the koji owner sync everytime a scm request is process.

---
+++ roles/bodhi2/backend/files/koji-sync-fedwatch-glue.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+""" This is a glue script to run /usr/local/bin/owner-sync-pagure on a given
+package anytime a ticket gets closed at
+https://pagure.io/releng/fedora-scm-requests
+
+Author: Ralph Bean <rbean@redhat.com>
+"""
+
+import json
+import subprocess as sp
+import sys
+
+
+def handle(content):
+    body = content.strip('`').strip()
+    body = json.loads(body)
+    package = body['repo']
+    # XXX If you modify this taglist.  Please also modify the other copy in
+    # bodhi2/backend/tasks/main.yml
+    taglist = 'f27 f26 f25 f27-container f26-container f25-container epel7 dist-6E-epel module-package-list'
+    cmd = [
+        '/usr/local/bin/owner-sync-pagure',
+        '--package', package,
+        '--verbose',
+    ] + taglist.split()
+    sp.Popen(cmd)
+
+
+def main(fullname, fields, content):
+    if fullname != 'releng/fedora-scm-requests':
+        print("Dropping %r.  Not scm request.")
+        return False
+    if 'close_status' not in fields:
+        print("Dropping %r.  Not closed.")
+        return False
+
+    handle(content)
+
+
+if __name__ == '__main__':
+    fullname, fields, content = sys.argv[-3:]
+    main(fullname, fields, content)
