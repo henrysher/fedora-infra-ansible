c2b469f8de8a68080bf7042aaba0c3d73d89100b
Dusty Mabe <dusty@dustymabe.com>
2017-09-12 17:24:52 -0400

    robosig: add in commit signing for f27
    Signed-off-by: Dusty Mabe <dusty@dustymabe.com>

--- roles/robosignatory/files/robosignatory.production.py
+++ roles/robosignatory/files/robosignatory.production.py
@@ -175,6 +175,22 @@ config = {
             'directory': '/mnt/fedora_koji/koji/compose/ostree/26/',
             'key': 'fedora-26'
         },
+        'fedora/27/x86_64/atomic-host': {
+            'directory': '/mnt/fedora_koji/koji/compose/atomic/27/',
+            'key': 'fedora-27'
+        },
+        'fedora/27/ppc64le/atomic-host': {
+            'directory': '/mnt/fedora_koji/koji/compose/atomic/27/',
+            'key': 'fedora-27'
+        },
+        'fedora/27/aarch64/atomic-host': {
+            'directory': '/mnt/fedora_koji/koji/compose/atomic/27/',
+            'key': 'fedora-27'
+        },
+        'fedora/27/x86_64/workstation': {
+            'directory': '/mnt/fedora_koji/koji/compose/ostree/27/',
+            'key': 'fedora-27'
+        },
         'fedora/rawhide/x86_64/atomic-host': {
             'directory': '/mnt/fedora_koji/koji/compose/atomic/rawhide/',
             'key': 'fedora-27'
