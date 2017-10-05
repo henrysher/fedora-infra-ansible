99ab900710ffc530161773760cc681612fa5b1aa
Patrick Uiterwijk <puiterwijk@redhat.com>
2017-10-05 10:47:54 +0000

    Define transitional-datanommer
    Signed-off-by: Patrick Uiterwijk <puiterwijk@redhat.com>

--- roles/fedmsg/datanommer/templates/datanommer.py
+++ roles/fedmsg/datanommer/templates/datanommer.py
@@ -1,7 +1,9 @@
 # Configuration for the datanommer consumer.  A plugin for the fedmsg-hub process.
 config = {
     'datanommer.enabled': True,
-{% if env == "production" %}
+{% if 'transitional' in inventory_hostname %}
+    'datanommer.sqlalchemy.url': 'postgresql://{{ transitionalDatanommerDBUser }}:{{ transitionalDatanommerDBPassword }}@db-datanommer01/datanommer',
+{% elif env == "production" %}
     'datanommer.sqlalchemy.url': 'postgresql://{{ datanommerDBUser }}:{{ datanommerDBPassword }}@db-datanommer02/datanommer',
 {% else %}
     'datanommer.sqlalchemy.url': 'postgresql://{{ datanommerDBUser }}:{{ datanommerDBPassword }}@db-datanommer.stg.phx2.fedoraproject.org/datanommer',
