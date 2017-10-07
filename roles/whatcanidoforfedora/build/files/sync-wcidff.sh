b9b41189ea94e36b41b9a466b4cbb43635a00797
Ralph Bean <rbean@redhat.com>
2017-10-07 09:34:01 +0000

    A stab at building whatcanidoforfedora.org in stg.

---
+++ roles/whatcanidoforfedora/build/files/sync-wcidff.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+# Borrowed heavily from the syncStatic cronjob script.
+
+if [ ! -d  /srv/web/whatcanidoforfedora.org/.git ]
+then
+    /usr/bin/git clone -q \
+        https://github.com/fedora-infra/asknot-ng.git \
+        /srv/web/whatcanidoforfedora.org
+fi
+
+cd /srv/web/whatcanidoforfedora.org
+
+/usr/bin/git clean -q -fdx || exit 1
+/usr/bin/git reset -q --hard || exit 1
+/usr/bin/git checkout -q develop || exit 1
+/usr/bin/git pull -q --ff-only || exit 1
+
+err=$(
+    {
+        ./build.sh &&\
+        rsync -qa --delete-after --delay-updates build/ "/srv/web/$site/"; \
+    } 2>&1
+)
+
+rc=$?
+if [ $rc -ne 0 ]; then
+    echo "$site build failed"
+    echo "===================================="
+    echo "$err"
+    echo
+fi
