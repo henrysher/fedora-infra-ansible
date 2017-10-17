50b92d5e54d40f3f7cf7738f43ad54505c5d51bf
Patrick Uiterwijk <puiterwijk@redhat.com>
2017-10-17 22:36:16 +0000

    Nuke transitional datanommer and new db-datanommer01
    Signed-off-by: Patrick Uiterwijk <puiterwijk@redhat.com>

--- roles/fedmsg/datanommer/templates/datanommer.py
+++ roles/fedmsg/datanommer/templates/datanommer.py
@@ -1,9 +1,7 @@
 # Configuration for the datanommer consumer.  A plugin for the fedmsg-hub process.
 config = {
     'datanommer.enabled': True,
-{% if 'transitional' in inventory_hostname %}
-    'datanommer.sqlalchemy.url': 'postgresql://{{ transitionalDatanommerDBUser }}:{{ transitionalDatanommerDBPassword }}@db-datanommer01/datanommer',
-{% elif env == "production" %}
+{% if env == "production" %}
     'datanommer.sqlalchemy.url': 'postgresql://{{ datanommerDBUser }}:{{ datanommerDBPassword }}@db-datanommer02/datanommer',
 {% else %}
     'datanommer.sqlalchemy.url': 'postgresql://{{ datanommerDBUser }}:{{ datanommerDBPassword }}@db-datanommer.stg.phx2.fedoraproject.org/datanommer',
