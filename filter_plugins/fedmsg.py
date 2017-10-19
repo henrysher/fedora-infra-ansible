e9cbb411eced4a875f7350b5cb49a50389f36431
Ralph Bean <rbean@redhat.com>
2017-10-19 15:19:40 +0000

    Try adding greenwave and waiverdb to the stg fedmsg routing policy.

--- filter_plugins/fedmsg.py
+++ filter_plugins/fedmsg.py
@@ -9,9 +9,9 @@ def invert_fedmsg_policy(groups, vars, env):
     """
 
     if env == 'staging':
-        hosts = groups['staging'] + groups['fedmsg-qa-network-stg']
+        hosts = groups['staging'] + groups['fedmsg-qa-network-stg'] + groups['openshift-pseudohosts-stg']
     else:
-        hosts = [h for h in groups['all'] if h not in groups['staging']]
+        hosts = [h for h in groups['all'] if h not in groups['staging'] + groups['openshift-pseudohosts-stg']]
 
     inverted = {}
     for host in hosts:
