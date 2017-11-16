86e40cf9e3a8639ebdbdb915920945b813c4d98f
Jeremy Cline <jeremy@jcline.org>
2017-11-16 20:27:49 +0000

    Don't overwrite default fedmsg log file

--- roles/notifs/backend/templates/fmn.consumer.py
+++ roles/notifs/backend/templates/fmn.consumer.py
@@ -157,9 +157,24 @@ config = {
                 "propagate": False,
                 "handlers": ["console", "mailer"],
             },
+            moksha={
+                "level": "INFO",
+                "propagate": False,
+                "handlers": ["console", "mailer"],
+            },
+            celery={
+                "level": "INFO",
+                "propagate": False,
+                "handlers": ["console", "mailer"],
+            },
+            twisted={
+                "level": "INFO",
+                "propagate": False,
+                "handlers": ["console", "mailer"],
+            },
         ),
         root={
-            'level': 'INFO',
+            'level': 'WARNING',
              'handlers': ['console', 'mailer'],
         },
     ),
