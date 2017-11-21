4d29d49b2dc997d79578b86185f01d9ba0d21201
Pierre-Yves Chibon <pingou@pingoured.fr>
2017-11-21 16:01:34 +0100

    Start working on the simple-koji-ci playbook

---
+++ roles/simple-koji-ci/templates/simple-koji-ci.py
@@ -0,0 +1,43 @@
+config = {
+    'simple-koji-ci.enabled': True,
+
+    'simple-koji-ci.koji': {
+        'server': 'https://koji.stg.fedoraproject.org/kojihub',
+        'weburl': 'https://koji.stg.fedoraproject.org/koji',
+        # Kerberos configuration to authenticate with Koji. In development
+        # environments, use `kinit <fas-name>@FEDORAPROJECT.ORG` to get a
+        # Kerberos ticket and use the default settings below.
+        'krb_principal': None,
+        'krb_keytab': None,
+        'krb_ccache': None,
+        'krb_proxyuser': None,
+        'krb_sessionopts': {'timeout': 3600, 'krb_rdns': False},
+        'git_url': 'https://src.stg.fedoraproject.org/rpms/{package}.git',
+        'opts': {'scratch': True},
+        'priority': 30,
+        'target_tag': 'rawhide',
+
+    },
+
+    "simple-koji-ci.cache": {
+        "backend": "dogpile.cache.dbm",
+        "expiration_time": 300,
+        "arguments": {
+            "filename": "/var/tmp/simple-koji-ci-cache.dbm",
+        },
+    },
+
+    "simple-koji-ci.pagure_url" : "https://src.stg.fedoraproject.org",
+    "simple-koji-ci.pagure_token" : "{{ simple_koji_ci_pagure_token_stg }}",
+
+    # The time in seconds the-new-hotness should wait for a socket to connect
+    # before giving up.
+    'simple-koji-ci.connect_timeout': 15,
+    # The time in seconds the-new-hotness should wait for a read from a socket
+    # before giving up.
+    'simple-koji-ci.read_timeout': 15,
+    # The number of times the-new-hotness should retry a network request that
+    # that failed for any reason (e.g. read timeout, DNS error, etc)
+    'simple-koji-ci.requests_retries': 3,
+}
+
