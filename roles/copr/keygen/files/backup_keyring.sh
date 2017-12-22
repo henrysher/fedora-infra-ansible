747faacf4ef768469287b4ad8a3b5a23557f47c9
Kevin Fenzi <kevin@scrye.com>
2017-12-22 17:14:58 +0000

    Fix excluding agent sockets.

--- roles/copr/keygen/files/backup_keyring.sh
+++ roles/copr/keygen/files/backup_keyring.sh
@@ -6,6 +6,6 @@
 PATH_TO_KEYRING_DIR="/var/lib/copr-keygen"
 OUTPUT_FILE="/backup/copr_keygen_keyring.tar.gz.gpg"
 
-tar --exclude="*agent" -czPf - $PATH_TO_KEYRING_DIR |
+tar --exclude="*agent*" -czPf - $PATH_TO_KEYRING_DIR |
   gpg2 --output $OUTPUT_FILE.tmp --encrypt --recipient admin@fedoraproject.org --always-trust &&
   mv $OUTPUT_FILE.tmp $OUTPUT_FILE
