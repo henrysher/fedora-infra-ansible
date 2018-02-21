87dc6e0e9ea4d37d59828389c904f84460a3355b
Aurélien Bompard <aurelien@bompard.org>
2018-02-21 10:02:54 +0000

    Hubs: improve gunicorn config

---
+++ roles/hubs/templates/gunicorn.py
@@ -0,0 +1,6 @@
+bind = "127.0.0.1:8000"
+threads = 12
+logconfig = "{{ hubs_conf_dir }}/logging.ini"
+accesslog = "{{ hubs_log_dir }}/access.log"
+errorlog = "{{ hubs_log_dir }}/error.log"
+access_log_format = '%(h)s %(l)s %(u)s %(t)s "%(r)s" %(s)s %(b)s "%(f)s" "%(a)s" (%(L)ss)'
