f5760b6f5ef132f1fd398cd1b031967571d608c4
Jan Kaluža <jkaluza@redhat.com>
2018-03-07 08:57:58 +0000

    Do exit with 1 in case service is not present while running conditional-restart.sh.

--- roles/base/files/common-scripts/conditional-restart.sh
+++ roles/base/files/common-scripts/conditional-restart.sh
@@ -7,4 +7,4 @@
 
 SERVICE=$1
 # Check if service unit is present before trying to restart it
-/usr/bin/systemctl cat $1.service &>/dev/null && /usr/bin/systemctl try-restart $1
+/usr/bin/systemctl cat $1.service &>/dev/null && /usr/bin/systemctl try-restart $1 || true
