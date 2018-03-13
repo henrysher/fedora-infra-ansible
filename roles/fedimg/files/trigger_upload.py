f71f59f4e3d8b1c011650c0113e521528bd6b1dc
Sayan Chowdhury <sayan.chowdhury2012@gmail.com>
2018-03-13 17:07:53 +0530

    fedimg: Fix the trigger_upload script to create AMIs manually
    Signed-off-by: Sayan Chowdhury <sayan.chowdhury2012@gmail.com>

--- roles/fedimg/files/trigger_upload.py
+++ roles/fedimg/files/trigger_upload.py
@@ -16,8 +16,8 @@ from fedimg.services.ec2 import EC2Service, EC2ServiceException
 import fedimg.uploader
 from fedimg.util import virt_types_from_url
 
-if len(sys.argv) != 2:
-    print 'Usage: trigger_upload.py <rawxz_image_url>'
+if len(sys.argv) != 3:
+    print 'Usage: trigger_upload.py <rawxz_image_url> <compose_id>'
     sys.exit(1)
 
 logging.config.dictConfig(fedmsg.config.load_config()['logging'])
@@ -26,5 +26,10 @@ log = logging.getLogger('fedmsg')
 upload_pool = multiprocessing.pool.ThreadPool(processes=4)
 
 url = sys.argv[1]
+compose_id = sys.argv[2]
 
-fedimg.uploader.upload(upload_pool, [url])
+compose_meta = {
+    'compose_id': compose_id
+}
+
+fedimg.uploader.upload(upload_pool, [url], compose_meta=compose_meta)
