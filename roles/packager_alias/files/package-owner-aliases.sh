73de9829d0afc8366af1adfd176fdaa235dac641
Jason Tibbitts <tibbs@math.uh.edu>
2018-07-25 11:00:20 -0500

    Add a -maintainers alias file.
    The current scripting generates aliases of the form
    foo-owner@fedoraproject.org.  But this conflicts with our messaging
    which says that the project "owns" packages and people "maintain" them.
    Plus, "owner" is singular while we really want to have more than one
    maintainer.
    
    This simply adds a second alias file generated from the original file.

--- roles/packager_alias/files/package-owner-aliases.sh
+++ roles/packager_alias/files/package-owner-aliases.sh
@@ -1,15 +1,25 @@
 #!/bin/bash
 
 output=`mktemp`
+moutput=`mktemp`
 dest=/etc/postfix/package-owner
+mdest=/etc/postfix/package-maintainers
 /usr/local/bin/owner-email.py >> $output
 if [ $? != 0 ]; then
    echo "error creating owner-alias file" >&2
    exit 1
 fi
 
+sed -e 's/-owner: /-maintainers: /' $output > $moutput
+
 cp $output $dest
 chmod +r $output
 mv $output $dest
 /usr/sbin/restorecon /etc/postfix/package-owner
 /usr/sbin/postalias /etc/postfix/package-owner
+
+cp $moutput $mdest
+chmod +r $moutput
+mv $moutput $mdest
+/usr/sbin/restorecon /etc/postfix/package-maintainers
+/usr/sbin/postalias /etc/postfix/package-maintainers
