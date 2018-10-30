442e3a2e8e1d8e0fe8a0a02595b47df593c38f89
Pierre-Yves Chibon <pingou@pingoured.fr>
2018-10-30 15:13:24 +0100

    Add the ci_resultsdb role
    Signed-off-by: Pierre-Yves Chibon <pingou@pingoured.fr>

---
+++ roles/ci_resultsdb/templates/ci-resultsdb-listener.py
@@ -0,0 +1,11 @@
+config = {
+    'resultsdb_listener.pipelinepackageignoreconsumer.enabled': True,
+    'resultsdb_listener.pipelineimagecompleteconsumer.enabled': True,
+    'resultsdb_listener.pipelinecomposecompleteconsumer.enabled': True,
+    'resultsdb_listener.pipelinepackagecompleteconsumer.enabled': True,
+    'resultsdb_listener.pipelineimagetestsmokecompleteconsumer.enabled': True,
+    'resultsdb_listener.pipelinepackagetestfunctionalcompleteconsumer.enabled': True,
+    'resultsdb_listener.pipelinecomposetestintegrationcompleteconsumer.enabled': True,
+    'resultsdb_listener.pipelinecompleteconsumer.enabled': True,
+    'resultsdb_listener.enabled': True,
+}
