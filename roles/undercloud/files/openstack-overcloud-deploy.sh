c2cf00435d0af2419194b229e8af4f975dd92d11
Kevin Fenzi <kevin@scrye.com>
2018-11-14 23:06:51 +0000

    Add a bunch more openstack junk. Some of it incomplete yet.

--- roles/undercloud/files/openstack-overcloud-deploy.sh
+++ roles/undercloud/files/openstack-overcloud-deploy.sh
@@ -3,12 +3,11 @@
 openstack overcloud deploy --templates \
   -e /home/stack/templates/node-info.yaml\
   -e /home/stack/templates/overcloud_images.yaml \
-  -e /usr/share/openstack-tripleo-heat-templates/environments/network-isolation.yaml \
   -r /home/stack/templates/roles_data.yaml \
   --ntp-server cloud-noc01.cloud.fedoraproject.org \
   --config-download \
   -e /usr/share/openstack-tripleo-heat-templates/environments/config-download-environment.yaml \
   --overcloud-ssh-user heat-admin \
-  --overcloud-ssh-key ~/.ssh/id_rsa
-
-#  -e /home/stack/templates/network-environment.yaml \
+  --overcloud-ssh-key ~/.ssh/id_rsa \
+  -e /home/stack/templates/cinder-dellps-config.yaml \
+  -e /home/stack/templates/rhel-registration/environment-rhel-registration.yaml
