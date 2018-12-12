1b3bda28c8b4fa68d8960744ead1e8b148c1ba2e
Kevin Fenzi <kevin@scrye.com>
2018-12-12 21:28:42 +0000

    Update undercloud role to reflect last few weeks of changes.
    * Nuke federation stuff, we are doing it manually.
    * update deploy for what we currently are using
    * drop cinder-dellps-config in favor of custom storage
    * Update nodes for ppc and ip changes and dropping fed-cloud-14/node14

--- roles/undercloud/files/openstack-overcloud-deploy.sh
+++ roles/undercloud/files/openstack-overcloud-deploy.sh
@@ -1,13 +1,18 @@
-#!/bin/bash
+!/bin/bash
 
 openstack overcloud deploy --templates \
   -e /home/stack/templates/node-info.yaml\
   -e /home/stack/templates/overcloud_images.yaml \
   -r /home/stack/templates/roles_data.yaml \
   --ntp-server cloud-noc01.cloud.fedoraproject.org \
-  --config-download \
-  -e /usr/share/openstack-tripleo-heat-templates/environments/config-download-environment.yaml \
+  -e /home/stack/templates/custom-storage.yaml \
   --overcloud-ssh-user heat-admin \
   --overcloud-ssh-key ~/.ssh/id_rsa \
-  -e /home/stack/templates/cinder-dellps-config.yaml \
-  -e /home/stack/templates/rhel-registration/environment-rhel-registration.yaml
+  -e /home/stack/templates/custom_domain.yaml \
+  --timeout 1800 \
+  --validation-errors-nonfatal
+
+#  --config-download \
+#  -e /usr/share/openstack-tripleo-heat-templates/environments/config-download-environment.yaml \
+#  -e /home/stack/templates/rhel-registration/environment-rhel-registration.yaml \
+#  -e /home/stack/templates/rhel-registration/rhel-registration-resource-registry.yaml \
