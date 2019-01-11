a3b14f235c84f7d087a8f1453f8e740373a9caf8
Rick Elrod <relrod@redhat.com>
2019-01-11 21:28:54 +0000

    try it this way
    Signed-off-by: Rick Elrod <relrod@redhat.com>

--- roles/dns/files/GeoIP.sh
+++ roles/dns/files/GeoIP.sh
@@ -10,4 +10,4 @@ rm -f GeoLite2-Country-CSV.zip csvs/*
 
 wget -q -T 5 -t 1 https://geolite.maxmind.com/download/geoip/database/GeoLite2-Country-CSV.zip
 unzip -j GeoLite2-Country-CSV.zip -d csvs/
-python geoip.py csvs/GeoLite2-Country-Blocks-IPv4.csv csvs/GeoLite2-Country-Blocks-IPv6.csv csvs/GeoLite2-Country-Locations-en.csv > /var/named/GeoIP.acl
+geoip.py csvs/GeoLite2-Country-Blocks-IPv4.csv csvs/GeoLite2-Country-Blocks-IPv6.csv csvs/GeoLite2-Country-Locations-en.csv > /var/named/GeoIP.acl
