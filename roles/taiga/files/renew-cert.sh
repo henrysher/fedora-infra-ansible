c71a31b220c53e02bd7a6c652cf8acf80a21e81e
Kevin Fenzi <kevin@scrye.com>
2019-05-02 22:03:09 +0000

    taiga: retire old taiga cloud instances, everything should be using teams.fedoraproject.org now.
    Signed-off-by: Kevin Fenzi <kevin@scrye.com>

--- roles/taiga/files/renew-cert.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-#renew letsencrypt certificate. it checks if cert need renewal. if not nginx will not stop
-# check and renew if required, quietly. if so do it in standalone mode
-/usr/bin/certbot renew -q --pre-hook "/usr/bin/systemctl stop nginx" --post-hook "/usr/bin/systemctl start nginx"
-
--- roles/taiga/files/urls.py
+++
@@ -1,4 +0,0 @@
-from taiga.urls import *
-urlpatterns += [
-    url(r"^api/oidc/", include("mozilla_django_oidc.urls")),
-]
