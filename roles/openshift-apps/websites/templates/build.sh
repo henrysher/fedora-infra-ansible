0c61eba755da17c490ebdba43519658b778716a3
Patrick Uiterwijk <patrick@puiterwijk.org>
2019-05-13 11:38:10 +0200

    website: Before building, log which revision we are building
    Signed-off-by: Patrick Uiterwijk <patrick@puiterwijk.org>

--- roles/openshift-apps/websites/templates/build.sh
+++ roles/openshift-apps/websites/templates/build.sh
@@ -2,4 +2,5 @@
 cd /tmp
 git clone --branch {% if env == "staging" %}staging{% else %}master{% endif %} https://pagure.io/fedora-web/websites.git
 cd websites
+git rev-parse HEAD
 exec ./build-prod.sh /output
