ef0c1a2ab4c54196a51e89f5d8ce671d8db274c8
Rick Elrod <relrod@redhat.com>
2019-05-23 17:32:53 +0000

    getfedora: move atomic htaccess stuff to openshift
    Signed-off-by: Rick Elrod <relrod@redhat.com>

--- roles/fedora-web/build/files/sync-atomic-htaccess.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-set -e
-cd /srv/web/atomic-htaccess-generator
-python /usr/local/bin/atomic-htaccess-generator.py
-# The . here is meaningful!
-cp -r out/. /srv/websites/getfedora.org/
--- roles/openshift-apps/websites/templates/build.sh
+++ roles/openshift-apps/websites/templates/build.sh
@@ -3,4 +3,6 @@ cd /tmp
 git clone --branch {% if env == "staging" %}staging{% else %}master{% endif %} https://pagure.io/fedora-web/websites.git
 cd websites
 git rev-parse HEAD
-exec ./build-prod.sh /output
+bash ./build-prod.sh /output
+python generate-htaccess.py
+mv out/. /output/
