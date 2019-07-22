dd9acb4f14d209bd2e57e38fef365b763776979d
Pierre-Yves Chibon <pingou@pingoured.fr>
2019-07-22 14:29:15 +0200

    waiverdb: Enable sending notification on the bus in prod
    Signed-off-by: Pierre-Yves Chibon <pingou@pingoured.fr>

--- roles/openshift-apps/waiverdb/templates/settings.py
+++ roles/openshift-apps/waiverdb/templates/settings.py
@@ -2,13 +2,12 @@
 DATABASE_URI = 'postgresql+psycopg2://waiverdb@db01.stg.phx2.fedoraproject.org:5432/waiverdb'
 RESULTSDB_API_URL = 'https://taskotron.stg.fedoraproject.org/resultsdb_api/api/v2.0'
 CORS_URL = 'https://bodhi.stg.fedoraproject.org'
-MESSAGE_BUS_PUBLISH = True
 {% else %}
 DATABASE_URI = 'postgresql+psycopg2://waiverdb@db01.phx2.fedoraproject.org:5432/waiverdb'
 RESULTSDB_API_URL = 'https://taskotron.fedoraproject.org/resultsdb_api/api/v2.0'
 CORS_URL = 'https://bodhi.fedoraproject.org'
-MESSAGE_BUS_PUBLISH = False
 {% endif %}
+MESSAGE_BUS_PUBLISH = True
 AUTH_METHOD = 'OIDC'
 OIDC_REQUIRED_SCOPE = 'https://waiverdb.fedoraproject.org/oidc/create-waiver'
 OIDC_CLIENT_SECRETS = '/etc/secret/client_secrets.json'
