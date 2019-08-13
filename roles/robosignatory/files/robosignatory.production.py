d1c2783c536f740091a5f0f43ef6c55cafc186ab
Kevin Fenzi <kevin@scrye.com>
2019-08-13 16:21:02 +0000

    autosign: remove eol releases and reorder
    Signed-off-by: Kevin Fenzi <kevin@scrye.com>

--- roles/robosignatory/files/robosignatory.production.py
+++ roles/robosignatory/files/robosignatory.production.py
@@ -38,12 +38,6 @@ config = {
             'tags': [
                 # Temporary tags
                 {
-                    "from": "f31-rebuild",
-                    "to": "f31-rebuild",
-                    "key": "fedora-31",
-                    "keyid": "3c3359c4"
-                },
-                {
                     "from": "f31-kde",
                     "to": "f31-kde",
                     "key": "fedora-31",
@@ -56,40 +50,16 @@ config = {
                     "keyid": "3c3359c4"
                 },
                 {
-                    "from": "f31-ghc",
-                    "to": "f31-ghc",
-                    "key": "fedora-31",
-                    "keyid": "3c3359c4"
-                },
-                {
-                    "from": "f31-go",
-                    "to": "f31-go",
-                    "key": "fedora-31",
-                    "keyid": "3c3359c4"
-                },
-                {
-                    "from": "f29-kde",
-                    "to": "f29-kde",
-                    "key": "fedora-29",
-                    "keyid": "429476b4"
-                },
-                {
                     "from": "f30-kde",
                     "to": "f30-kde",
                     "key": "fedora-30",
                     "keyid": "cfc659b9"
                 },
                 {
-                    "from": "f30-ghc",
-                    "to": "f30-ghc",
-                    "key": "fedora-30",
-                    "keyid": "cfc659b9"
-                },
-                {
-                    "from": "f30-gnome",
-                    "to": "f30-gnome",
-                    "key": "fedora-30",
-                    "keyid": "cfc659b9"
+                    "from": "f29-kde",
+                    "to": "f29-kde",
+                    "key": "fedora-29",
+                    "keyid": "429476b4"
                 },
                 # Infra tags
                 {
@@ -107,24 +77,6 @@ config = {
                 {
                     "from": "epel8-infra-candidate",
                     "to": "epel8-infra-stg",
-                    "key": "fedora-infra",
-                    "keyid": "47dd8ef9"
-                },
-                {
-                    "from": "f26-infra-candidate",
-                    "to": "f26-infra-stg",
-                    "key": "fedora-infra",
-                    "keyid": "47dd8ef9"
-                },
-                {
-                    "from": "f27-infra-candidate",
-                    "to": "f27-infra-stg",
-                    "key": "fedora-infra",
-                    "keyid": "47dd8ef9"
-                },
-                {
-                    "from": "f28-infra-candidate",
-                    "to": "f28-infra-stg",
                     "key": "fedora-infra",
                     "keyid": "47dd8ef9"
                 },
@@ -250,49 +202,6 @@ config = {
                     "keyid": "429476b4"
                 },
                 {
-                    "from": "f28-modular-signing-pending",
-                    "to": "f28-modular-updates-testing-pending",
-                    "key": "fedora-28",
-                    "keyid": "9db62fb1",
-                    "type": "modular"
-                },
-                {
-                    "from": "f28-signing-pending",
-                    "to": "f28-updates-testing-pending",
-                    "key": "fedora-28",
-                    "keyid": "9db62fb1"
-                },
-                {
-                    "from": "f27-signing-pending",
-                    "to": "f27-updates-testing-pending",
-                    "key": "fedora-27",
-                    "keyid": "f5282ee4"
-                },
-                {
-                    "from": "f26-signing-pending",
-                    "to": "f26-updates-testing-pending",
-                    "key": "fedora-26",
-                    "keyid": "64dab85d"
-                },
-                {
-                    "from": "f25-signing-pending",
-                    "to": "f25-updates-testing-pending",
-                    "key": "fedora-25",
-                    "keyid": "fdb19c98"
-                },
-                {
-                    "from": "f24-signing-pending",
-                    "to": "f24-updates-testing-pending",
-                    "key": "fedora-24",
-                    "keyid": "81b46521"
-                },
-                {
-                    "from": "f23-signing-pending",
-                    "to": "f23-updates-testing-pending",
-                    "key": "fedora-23",
-                    "keyid": "34ec9cba"
-                },
-                {
                     "from": "epel8-signing-pending",
                     "to": "epel8-testing-pending",
                     "key": "epel-8",
@@ -317,12 +226,6 @@ config = {
                     "to": "dist-6E-epel-testing-candidate",
                     "key": "epel-6",
                     "keyid": "0608b895"
-                },
-                {
-                    "from": "dist-5E-epel-testing-candidate",
-                    "to": "dist-5E-epel-testing-candidate",
-                    "key": "epel-5",
-                    "keyid": "217521f6"
                 },
             ],
         },
@@ -400,118 +303,6 @@ config = {
         'fedora/29/armhfp/iot': {
             'directory': '/mnt/fedora_koji/koji/compose/iot/repo/',
             'key': 'fedora-29'
-        },
-        'fedora/28/x86_64/iot': {
-            'directory': '/mnt/fedora_koji/koji/compose/iot/repo/',
-            'key': 'fedora-28'
-        },
-        'fedora/28/aarch64/iot': {
-            'directory': '/mnt/fedora_koji/koji/compose/iot/repo/',
-            'key': 'fedora-28'
-        },
-        'fedora/28/armhfp/iot': {
-            'directory': '/mnt/fedora_koji/koji/compose/iot/repo/',
-            'key': 'fedora-28'
-        },
-        'fedora/26/x86_64/testing/atomic-host': {
-            'directory': '/mnt/fedora_koji/koji/compose/ostree/repo/',
-            'key': 'fedora-26'
-        },
-        'fedora/26/x86_64/updates/atomic-host': {
-            'directory': '/mnt/fedora_koji/koji/compose/ostree/repo/',
-            'key': 'fedora-26'
-        },
-        'fedora/26/x86_64/atomic-host': {
-            'directory': '/mnt/fedora_koji/koji/compose/ostree/repo/',
-            'key': 'fedora-26'
-        },
-        'fedora/26/x86_64/workstation': {
-            'directory': '/mnt/fedora_koji/koji/compose/ostree/repo/',
-            'key': 'fedora-26'
-        },
-        'fedora/27/x86_64/updates/workstation': {
-            'directory': '/mnt/fedora_koji/koji/compose/ostree/repo/',
-            'key': 'fedora-27'
-        },
-        'fedora/27/x86_64/testing/workstation': {
-            'directory': '/mnt/fedora_koji/koji/compose/ostree/repo/',
-            'key': 'fedora-27'
-        },
-        'fedora/27/x86_64/updates/atomic-host': {
-            'directory': '/mnt/fedora_koji/koji/compose/ostree/repo/',
-            'key': 'fedora-27'
-        },
-        'fedora/27/ppc64le/updates/atomic-host': {
-            'directory': '/mnt/fedora_koji/koji/compose/ostree/repo/',
-            'key': 'fedora-27'
-        },
-        'fedora/27/aarch64/updates/atomic-host': {
-            'directory': '/mnt/fedora_koji/koji/compose/ostree/repo/',
-            'key': 'fedora-27'
-        },
-        'fedora/27/x86_64/testing/atomic-host': {
-            'directory': '/mnt/fedora_koji/koji/compose/ostree/repo/',
-            'key': 'fedora-27'
-        },
-        'fedora/27/ppc64le/testing/atomic-host': {
-            'directory': '/mnt/fedora_koji/koji/compose/ostree/repo/',
-            'key': 'fedora-27'
-        },
-        'fedora/27/aarch64/testing/atomic-host': {
-            'directory': '/mnt/fedora_koji/koji/compose/ostree/repo/',
-            'key': 'fedora-27'
-        },
-        'fedora/27/x86_64/workstation': {
-            'directory': '/mnt/fedora_koji/koji/compose/ostree/repo/',
-            'key': 'fedora-27'
-        },
-        'fedora/28/x86_64/atomic-host': {
-            'directory': '/mnt/fedora_koji/koji/compose/ostree/repo/',
-            'key': 'fedora-28'
-        },
-        'fedora/28/ppc64le/atomic-host': {
-            'directory': '/mnt/fedora_koji/koji/compose/ostree/repo/',
-            'key': 'fedora-28'
-        },
-        'fedora/28/aarch64/atomic-host': {
-            'directory': '/mnt/fedora_koji/koji/compose/ostree/repo/',
-            'key': 'fedora-28'
-        },
-        'fedora/28/x86_64/updates/atomic-host': {
-            'directory': '/mnt/fedora_koji/koji/compose/ostree/repo/',
-            'key': 'fedora-28'
-        },
-        'fedora/28/ppc64le/updates/atomic-host': {
-            'directory': '/mnt/fedora_koji/koji/compose/ostree/repo/',
-            'key': 'fedora-28'
-        },
-        'fedora/28/aarch64/updates/atomic-host': {
-            'directory': '/mnt/fedora_koji/koji/compose/ostree/repo/',
-            'key': 'fedora-28'
-        },
-        'fedora/28/x86_64/testing/atomic-host': {
-            'directory': '/mnt/fedora_koji/koji/compose/ostree/repo/',
-            'key': 'fedora-28'
-        },
-        'fedora/28/ppc64le/testing/atomic-host': {
-            'directory': '/mnt/fedora_koji/koji/compose/ostree/repo/',
-            'key': 'fedora-28'
-        },
-        'fedora/28/aarch64/testing/atomic-host': {
-            'directory': '/mnt/fedora_koji/koji/compose/ostree/repo/',
-            'key': 'fedora-28'
-        },
-        'fedora/28/x86_64/workstation': {
-            'directory': '/mnt/fedora_koji/koji/compose/ostree/repo/',
-            'key': 'fedora-28'
-        },
-        'fedora/28/x86_64/updates/workstation': {
-            'directory': '/mnt/fedora_koji/koji/compose/ostree/repo/',
-            'key': 'fedora-28'
-        },
-        'fedora/28/x86_64/testing/workstation': {
-            'directory': '/mnt/fedora_koji/koji/compose/ostree/repo/',
-            'key': 'fedora-28'
         },
         'fedora/29/x86_64/atomic-host': {
             'directory': '/mnt/fedora_koji/koji/compose/ostree/repo/',
