6dc496d0d0e9bd9ebc43a78fbf5d4118c3de83e8
Chenxiong Qi <cqi@redhat.com>
2019-09-06 15:10:18 +0800

    message-tagging-service: load config content from file
    Config content are moved to single file to simplify configmap.yml
    
    Signed-off-by: Chenxiong Qi <cqi@redhat.com>

---
+++ roles/openshift-apps/message-tagging-service/templates/app-config.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+
+
+class BaseConfiguration:
+    log_level = 'DEBUG'
+
+    koji_cert = None
+    keytab = '/etc/krb5.keytab'
+    principal = '{{ app }}/{{ app }}{{ env_suffix }}.fedoraproject.org@{{ ipa_realm }}'
+
+    messaging_backend = 'fedora-messaging'
+    build_state = 'ready'
+
+    {% if env == 'staging' %}
+
+    dry_run = False
+    # Running in staging, a rule file inside my perosnal repo is used in order to test conveniently.
+    rules_file_url = 'https://pagure.io/mts-rules/raw/master/f/rules.yaml'
+    mbs_api_url = 'https://mbs.stg.fedoraproject.org/module-build-service/1/'
+    koji_profile = 'stg'
+
+    {% else %}
+
+    dry_run = False
+    rules_file_url = ('https://infrastructure.fedoraproject.org/cgit/ansible.git/tree/'
+                      'roles/openshift-apps/message-tagging-service/files/mts-rules.yml')
+    mbs_api_url = 'https://mbs.fedoraproject.org/module-build-service/1/'
+    koji_profile = 'koji'
+
+    {% endif %}
