768794f14e96244beab2a054390ac9041be16dbb
Kevin Fenzi <kevin@scrye.com>
2019-09-18 22:12:29 +0000

    distgit / fas override: add override for pkfed
    Signed-off-by: Kevin Fenzi <kevin@scrye.com>

--- roles/distgit/pagure/files/fas2.py
+++ roles/distgit/pagure/files/fas2.py
@@ -279,6 +279,8 @@ class AccountSystem(BaseClient):
             216452: 'dwd@fedoraproject.org',
             # Till Hofmann: <till.hofmann@posteo.de>
             175557: 'thofmann@fedoraproject.org',
+            # Philip Kovacs: pkfed@mxbits.io
+            218108: 'pkfed@fedoraproject.org',
         }
         # A few people have an email account that is used in owners.list but
         # have setup a bugzilla account for their primary account system email
