9dec538c455e75a220a37f71b4f1e47a5ea4cb04
Pavel Raiskup <praiskup@redhat.com>
2019-12-03 10:10:53 +0100

    copr: configure swap on aarch64 as well as on x86_64

--- roles/copr/backend/files/provision/files/enable-swap.sh
+++ roles/copr/backend/files/provision/files/enable-swap.sh
@@ -1,3 +1,15 @@
 #!/usr/bin/bash
-mkswap /dev/nvme0n1
-swapon /dev/nvme0n1
+
+set -e
+
+swap_device=
+if test -e /dev/xvda1 -a test -e /dev/nvme0n1; then
+    swap_device=/dev/nvme0n1
+elif test -e /dev/nvme1n1; then
+    swap_device=/dev/nvme1n1
+else
+
+test -n "$swap_device"
+
+mkswap "$swap_device"
+swapon "$swap_device"
