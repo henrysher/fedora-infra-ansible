dd919b1a588aaa47aa59da6c6f1fc0eca27ddbf9
Pavel Raiskup <praiskup@redhat.com>
2019-12-03 10:49:03 +0100

    copr: aws swapon typos

--- roles/copr/backend/files/provision/files/enable-swap.sh
+++ roles/copr/backend/files/provision/files/enable-swap.sh
@@ -3,11 +3,11 @@
 set -e
 
 swap_device=
-if test -e /dev/xvda1 -a test -e /dev/nvme0n1; then
+if test -e /dev/xvda1 && test -e /dev/nvme0n1; then
     swap_device=/dev/nvme0n1
 elif test -e /dev/nvme1n1; then
     swap_device=/dev/nvme1n1
-else
+fi
 
 test -n "$swap_device"
 
