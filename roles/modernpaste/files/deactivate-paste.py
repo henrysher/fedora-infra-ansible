23d953adf4d798d46b29f707c8d0be2f1fa6c561
Rick Elrod <relrod@redhat.com>
2019-12-03 16:32:25 +0000

    Get rid of modernpaste everywhere, redirect it to paste.centos.org everywhere
    Signed-off-by: Rick Elrod <relrod@redhat.com>

--- roles/modernpaste/files/deactivate-paste.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python
-import sys
-sys.path.append('/usr/share/modern-paste/app')
-import modern_paste
-from util.cryptography import get_decid
-from database.paste import deactivate_paste
-
-print(deactivate_paste(get_decid(sys.argv[1])))
